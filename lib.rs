70483c1a0c0870475f2a5c7a0c7aa3ac15aa3e8f
Steven Fackler <sfackler@gmail.com>
2013-12-28 20:18:08 -0700

    Add a crate_type attribute

--- lib.rs
+++ lib.rs
@@ -1,5 +1,6 @@
 #[feature(struct_variant, macro_rules)];
 #[crate_id="github.com/sfackler/rust-openssl"];
+#[crate_type="lib"];
 #[doc(html_root_url="http://sfackler.github.io/rust-openssl/doc")];
 
 extern mod extra;
