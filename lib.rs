37240b51f5c45f6d585b6ae0406eff6802dd6c17
Steven Fackler <sfackler@gmail.com>
2014-01-18 14:55:37 -0800

    Switch doc link to rust-ci

--- lib.rs
+++ lib.rs
@@ -1,7 +1,7 @@
 #[feature(struct_variant, macro_rules)];
 #[crate_id="github.com/sfackler/rust-openssl#openssl:0.0"];
 #[crate_type="lib"];
-#[doc(html_root_url="http://sfackler.github.io/rust-openssl/doc")];
+#[doc(html_root_url="http://www.rust-ci.org/sfackler/rust-openssl/doc")];
 
 extern mod extra;
 
