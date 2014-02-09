360fa1f04b575b2070f46611d9db3a852ab4a9d5
Steven Fackler <sfackler@gmail.com>
2014-02-09 15:49:16 -0800

    Build static and dynamic libs

--- lib.rs
+++ lib.rs
@@ -1,6 +1,7 @@
 #[feature(struct_variant, macro_rules)];
 #[crate_id="github.com/sfackler/rust-openssl#openssl:0.0"];
-#[crate_type="lib"];
+#[crate_type="rlib"];
+#[crate_type="dylib"];
 #[doc(html_root_url="http://www.rust-ci.org/sfackler/rust-openssl/doc")];
 
 extern mod extra;
