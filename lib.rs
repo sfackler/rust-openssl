2b48889466f292791c71839b217b0d2a84f114a9
Michael Gehring <mg@ebfe.org>
2014-07-06 11:58:41 +0200

    crate_id -> crate_name

--- lib.rs
+++ lib.rs
@@ -1,5 +1,5 @@
 #![feature(struct_variant, macro_rules)]
-#![crate_id="github.com/sfackler/rust-openssl#openssl:0.0"]
+#![crate_name="openssl"]
 #![crate_type="rlib"]
 #![crate_type="dylib"]
 #![doc(html_root_url="http://www.rust-ci.org/sfackler/rust-openssl/doc")]
