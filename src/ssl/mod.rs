5f017cd549b4c76849bfd5c33e6f6962acd89535
Jared Roesch <roeschinc@gmail.com>
2014-10-11 01:50:34 -0700

    Refactor init and error handling code
    Move common ffi initialization code to 'ffi::init()' and the initialization of error handling to a
    a shared location.

--- src/ssl/mod.rs
+++ src/ssl/mod.rs
@@ -23,9 +23,8 @@ fn init() {
 
     unsafe {
         INIT.doit(|| {
-            ffi::SSL_library_init();
-            ffi::SSL_load_error_strings();
-            ffi::ERR_load_crypto_strings();
+            ffi::init();
+
             let verify_idx = ffi::SSL_CTX_get_ex_new_index(0, ptr::null(), None,
                                                            None, None);
             assert!(verify_idx >= 0);
