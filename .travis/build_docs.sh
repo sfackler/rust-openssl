0894efc3ff7616f78324202369d1f05ff60aeb15
Steven Fackler <sfackler@gmail.com>
2015-02-07 21:48:03 -0800

    Fix stuff

--- .travis/build_docs.sh
+++ .travis/build_docs.sh
@@ -7,5 +7,5 @@ mkdir doc
 for crate in $(echo openssl-sys openssl); do
     mkdir -p $crate/target
     ln -s -t $crate/target ../../doc
-    (cd $crate && cargo doc --no-deps --features $FEATURES)
+    (cd $crate && cargo doc --no-deps --features "$FEATURES")
 done
