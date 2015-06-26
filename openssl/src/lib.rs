524c1e63aa759cd40e4f1efd6d20da3754616668
Steven Fackler <sfackler@gmail.com>
2015-06-25 23:22:04 -0700

    Release v0.6.3

--- openssl/src/lib.rs
+++ openssl/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url="https://sfackler.github.io/rust-openssl/doc")]
+#![doc(html_root_url="https://sfackler.github.io/rust-openssl/doc/v0.6.3")]
 
 #[macro_use]
 extern crate bitflags;
