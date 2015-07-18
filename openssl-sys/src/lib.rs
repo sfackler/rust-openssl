sfackler/rust-openssl

commit c8d23f37a40afbb7ccb768241e429a51f94b8e7a

Fix EOF handling in retry wrapper

openssl-sys/src/lib.rs
@@ -116,6 +116,7 @@ pub type PasswordCallback = extern "C" fn(buf: *mut c_char, size: c_int,
                                           -> c_int;
 
 pub const BIO_CTRL_EOF: c_int = 2;
+pub const BIO_C_SET_BUF_MEM_EOF_RETURN: c_int = 130;
 
 pub const CRYPTO_LOCK: c_int = 1;
 
@@ -269,6 +270,10 @@ pub unsafe fn BIO_eof(b: *mut BIO) -> bool {
 
 pub unsafe fn SSL_CTX_set_options(ssl: *mut SSL_CTX, op: c_long) -> c_long {
     SSL_CTX_ctrl(ssl, SSL_CTRL_OPTIONS, op, ptr::null_mut())
+}
+
+pub unsafe fn BIO_set_mem_eof_return(b: *mut BIO, v: c_int) {
+    BIO_ctrl(b, BIO_C_SET_BUF_MEM_EOF_RETURN, v as c_long, ptr::null_mut());
 }
 
 pub unsafe fn SSL_CTX_get_options(ssl: *mut SSL_CTX) -> c_long {

openssl/src/ssl/mod.rs
@@ -758,7 +758,7 @@ pub struct SslStream<S> {
 impl SslStream<net::TcpStream> {
     /// Create a new independently owned handle to the underlying socket.
     pub fn try_clone(&self) -> io::Result<SslStream<net::TcpStream>> {
-        Ok(SslStream { 
+        Ok(SslStream {
             stream: try!(self.stream.try_clone()),
             ssl: self.ssl.clone(),
             buf: self.buf.clone(),
@@ -851,14 +851,16 @@ impl<S: Read+Write> SslStream<S> {
                     try_ssl_stream!(self.flush());
                     let len = try_ssl_stream!(self.stream.read(&mut self.buf[..]));
                     if len == 0 {
-                        return Ok(0);
+                        self.ssl.get_rbio().set_eof(true);
+                    } else {
+                        try_ssl_stream!(self.ssl.get_rbio().write_all(&self.buf[..len]));
                     }
-                    try_ssl_stream!(self.ssl.get_rbio().write_all(&self.buf[..len]));
                 }
                 LibSslError::ErrorWantWrite => { try_ssl_stream!(self.flush()) }
                 LibSslError::ErrorZeroReturn => return Err(SslSessionClosed),
                 LibSslError::ErrorSsl => return Err(SslError::get()),
-                err => panic!("unexpected error {:?}", err),
+                LibSslError::ErrorSyscall if ret == 0 => return Ok(0),
+                err => panic!("unexpected error {:?} with ret {}", err, ret),
             }
         }
     }

openssl/src/ssl/tests.rs
@@ -369,7 +369,7 @@ fn test_pending() {
     let pending = stream.pending();
     let len = stream.read(&mut buf[1..]).unwrap();
     assert_eq!(pending, len);
-} 
+}
 
 /// Tests that connecting with the client using NPN, but the server not does not
 /// break the existing connection behavior.

openssl/src/bio/mod.rs
@@ -56,6 +56,12 @@ impl MemBio {
     pub unsafe fn get_handle(&self) -> *mut ffi::BIO {
         self.bio
     }
+
+    /// Sets the BIO's EOF state.
+    pub fn set_eof(&self, eof: bool) {
+        let v = if eof { 0 } else { -1 };
+        unsafe { ffi::BIO_set_mem_eof_return(self.bio, v); }
+    }
 }
 
 impl Read for MemBio {

commit a43011d77c26b0a371565ffe284611091cfd1de3

Fix probelms with DTLS when no packets are pending.
When using DTLS you might run into the situation where no packets
are pending, so SSL_read returns len=0. On a TLS connection this
means that the connection was closed, but on DTLS it does not
(a DTLS connection cannot be closed in the usual sense).
This commit fixes a bug introduced by c8d23f3.

Conflicts:
	openssl/src/ssl/mod.rs

openssl-sys/src/lib.rs
@@ -525,6 +525,7 @@ extern "C" {
     pub fn SSL_get_SSL_CTX(ssl: *mut SSL) -> *mut SSL_CTX;
     pub fn SSL_get_current_compression(ssl: *mut SSL) -> *const COMP_METHOD;
     pub fn SSL_get_peer_certificate(ssl: *mut SSL) -> *mut X509;
+    pub fn SSL_get_ssl_method(ssl: *mut SSL) -> *const SSL_METHOD;
 
     pub fn SSL_COMP_get_name(comp: *const COMP_METHOD) -> *const c_char;
 

openssl/src/ssl/mod.rs
@@ -141,6 +141,25 @@ impl SslMethod {
         }
     }
 
+    unsafe fn from_raw(method: *const ffi::SSL_METHOD) -> Option<SslMethod> {
+        match method {
+            #[cfg(feature = "sslv2")]
+            x if x == ffi::SSLv2_method() => Some(SslMethod::Sslv2),
+            x if x == ffi::SSLv3_method() => Some(SslMethod::Sslv3),
+            x if x == ffi::TLSv1_method() => Some(SslMethod::Tlsv1),
+            x if x == ffi::SSLv23_method() => Some(SslMethod::Sslv23),
+            #[cfg(feature = "tlsv1_1")]
+            x if x == ffi::TLSv1_1_method() => Some(SslMethod::Tlsv1_1),
+            #[cfg(feature = "tlsv1_2")]
+            x if x == ffi::TLSv1_2_method() => Some(SslMethod::Tlsv1_2),
+            #[cfg(feature = "dtlsv1")]
+            x if x == ffi::DTLSv1_method() => Some(SslMethod::Dtlsv1),
+            #[cfg(feature = "dtlsv1_2")]
+            x if x == ffi::DTLSv1_2_method() => Some(SslMethod::Dtlsv1_2),
+            _ => None,
+        }
+    }
+
     #[cfg(feature = "dtlsv1")]
     pub fn is_dtlsv1(&self) -> bool {
         *self == SslMethod::Dtlsv1
@@ -787,6 +806,13 @@ impl Ssl {
             ffi::SSL_pending(self.ssl) as usize
         }
     }
+
+    pub fn get_ssl_method(&self) -> Option<SslMethod> {
+        unsafe {
+            let method = ffi::SSL_get_ssl_method(self.ssl);
+            SslMethod::from_raw(method)
+        }
+    }
 }
 
 macro_rules! make_LibSslError {
@@ -888,8 +914,16 @@ impl<S: Read+Write> IndirectStream<S> {
                 LibSslError::ErrorWantRead => {
                     try_ssl_stream!(self.flush());
                     let len = try_ssl_stream!(self.stream.read(&mut self.buf[..]));
+
+
                     if len == 0 {
-                        self.ssl.get_rbio().set_eof(true);
+                        let method = self.ssl.get_ssl_method();
+
+                        if method.map(|m| m.is_dtls()).unwrap_or(false) {
+                            return Ok(0);
+                        } else {
+                            self.ssl.get_rbio().set_eof(true);
+                        }
                     } else {
                         try_ssl_stream!(self.ssl.get_rbio().write_all(&self.buf[..len]));
                     }

openssl/src/ssl/tests.rs
@@ -51,7 +51,7 @@ macro_rules! run_test(
             use std::net::TcpStream;
             use ssl;
             use ssl::SslMethod;
-            use ssl::{SslContext, SslStream, VerifyCallback};
+            use ssl::{SslContext, Ssl, SslStream, VerifyCallback};
             use ssl::SSL_VERIFY_PEER;
             use crypto::hash::Type::SHA256;
             use x509::X509StoreContext;
@@ -84,6 +84,11 @@ run_test!(new_ctx, |method, _| {
 
 run_test!(new_sslstream, |method, stream| {
     SslStream::connect_generic(&SslContext::new(method).unwrap(), stream).unwrap();
+});
+
+run_test!(get_ssl_method, |method, _| {
+    let ssl = Ssl::new(&SslContext::new(method).unwrap()).unwrap();
+    assert_eq!(ssl.get_ssl_method(), Some(method));
 });
 
 run_test!(verify_untrusted, |method, stream| {