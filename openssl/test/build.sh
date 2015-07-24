3fc8482d368530ff619cc580f9d4f16344cbb3c5
Steven Fackler <sfackler@gmail.com>
2015-07-23 21:50:49 -0700

    Make curl follow redirects

--- openssl/test/build.sh
+++ openssl/test/build.sh
@@ -4,7 +4,7 @@ set -e
 mkdir /tmp/openssl
 cd /tmp/openssl
 sudo apt-get install gcc make
-curl https://openssl.org/source/openssl-1.0.2-latest.tar.gz | tar --strip-components=1 -xzf -
+curl -L https://openssl.org/source/openssl-1.0.2-latest.tar.gz | tar --strip-components=1 -xzf -
 ./config --prefix=/usr/ shared
 make
 sudo make install
