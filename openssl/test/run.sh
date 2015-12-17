ccab187f5aa987d1c91bdbdef891e0d841ec1afd
Steven Fackler <sfackler@gmail.com>
2015-12-16 23:47:18 -0800

    Travis fixes

--- openssl/test/run.sh
+++ openssl/test/run.sh
@@ -3,17 +3,17 @@ set -e
 
 MAIN_TARGETS=https://static.rust-lang.org/dist
 
-if [ $TEST_FEATURES == "true" ]; then
+if [ "$TEST_FEATURES" == "true" ]; then
     FEATURES="tlsv1_2 tlsv1_1 dtlsv1 dtlsv1_2 sslv2 sslv3 aes_xts aes_ctr npn alpn rfc5114 ecdh_auto pkcs5_pbkdf2_hmac"
 fi
 
-if [ $TRAVIS_OS_NAME != "osx" ]; then
+if [ "$TRAVIS_OS_NAME" != "osx" ]; then
     export OPENSSL_LIB_DIR=$HOME/openssl/lib
     export OPENSSL_INCLUDE_DIR=$HOME/openssl/include
     export LD_LIBRARY_PATH=$HOME/openssl/lib:$LD_LIBRARY_PATH
 fi
 
-if [ $TARGET != "" ]; then
+if [ -n "$TARGET" ]; then
     FLAGS="--target=$TARGET"
     COMMAND="build"
 
