4f0a7e24d16dcdc7587293c35b227b9bd7963e2e
Steven Fackler <sfackler@gmail.com>
2015-12-18 22:20:57 -0800

    Drop verbose flag

--- build_docs.sh
+++ build_docs.sh
@@ -6,5 +6,5 @@ export CARGO_TARGET_DIR=target
 for toml in $(find . -maxdepth 2 -name "Cargo.toml"); do
     cargo update --manifest-path $toml || true
     features=$(cargo read-manifest --manifest-path $toml | jq -r '.features|keys|join(" ")')
-    cargo doc --verbose --no-deps --manifest-path $toml --features "$features"
+    cargo doc --no-deps --manifest-path $toml --features "$features"
 done
