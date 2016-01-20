36a667be49075ddea193214b55f76f6186c6f64a
Cody P Schafer <dev@codyps.com>
2016-01-19 22:04:18 -0500

    x509: impl Clone using references & CRYPTO_add()

--- openssl/src/c_helpers.c
+++ openssl/src/c_helpers.c
@@ -11,3 +11,7 @@ void rust_SSL_CTX_clone(SSL_CTX *ctx) {
 void rust_EVP_PKEY_clone(EVP_PKEY *pkey) {
     CRYPTO_add(&pkey->references,1,CRYPTO_LOCK_EVP_PKEY);
 }
+
+void rust_X509_clone(X509 *x509) {
+    CRYPTO_add(&x509->references,1,CRYPTO_LOCK_X509);
+}
