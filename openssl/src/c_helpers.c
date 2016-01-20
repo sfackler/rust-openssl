3c51f159a7f16c95061f51355536a66f77c6c63c
Cody P Schafer <dev@codyps.com>
2016-01-19 21:48:44 -0500

    crypto/pkey: impl Clone for PKey using openssl's ref counting

--- openssl/src/c_helpers.c
+++ openssl/src/c_helpers.c
@@ -7,3 +7,7 @@ void rust_SSL_clone(SSL *ssl) {
 void rust_SSL_CTX_clone(SSL_CTX *ctx) {
     CRYPTO_add(&ctx->references,1,CRYPTO_LOCK_SSL_CTX);
 }
+
+void rust_EVP_PKEY_clone(EVP_PKEY *pkey) {
+    CRYPTO_add(&pkey->references,1,CRYPTO_LOCK_EVP_PKEY);
+}
