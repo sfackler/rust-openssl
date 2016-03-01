2fe3e48487843dc55bb70b252aefd02459cd273a
Steven Fackler <sfackler@gmail.com>
2016-03-01 11:05:41 -0800

    Stop testing sslv2 feature on Travis
    OpenSSL removed support for this entirely in the most recent release.

--- openssl/test/run.sh
+++ openssl/test/run.sh
@@ -4,7 +4,7 @@ set -e
 MAIN_TARGETS=https://static.rust-lang.org/dist
 
 if [ "$TEST_FEATURES" == "true" ]; then
-    FEATURES="tlsv1_2 tlsv1_1 dtlsv1 dtlsv1_2 sslv2 sslv3 aes_xts aes_ctr npn alpn rfc5114 ecdh_auto pkcs5_pbkdf2_hmac"
+    FEATURES="tlsv1_2 tlsv1_1 dtlsv1 dtlsv1_2 sslv3 aes_xts aes_ctr npn alpn rfc5114 ecdh_auto pkcs5_pbkdf2_hmac"
 fi
 
 if [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
