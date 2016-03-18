a9a18cf337f5cf800332b4d4b2ab9d7c4eb5f77d
Steven Fackler <sfackler@gmail.com>
2016-03-17 22:23:51 -0700

    Simplify panic safety logic for new nightly

--- openssl/src/ssl/bio.rs
+++ openssl/src/ssl/bio.rs
@@ -74,42 +74,13 @@ unsafe fn state<'a, S: 'a>(bio: *mut BIO) -> &'a mut StreamState<S> {
 }
 
 #[cfg(feature = "nightly")]
-fn recover<F, T>(f: F) -> Result<T, Box<Any + Send>> where F: FnOnce() -> T + ::std::panic::RecoverSafe {
-    ::std::panic::recover(f)
+fn recover<F, T>(f: F) -> Result<T, Box<Any + Send>> where F: FnOnce() -> T {
+    ::std::panic::recover(::std::panic::AssertRecoverSafe::new(f))
 }
 
 #[cfg(not(feature = "nightly"))]
 fn recover<F, T>(f: F) -> Result<T, Box<Any + Send>> where F: FnOnce() -> T {
     Ok(f())
-}
-
-#[cfg(feature = "nightly")]
-use std::panic::AssertRecoverSafe;
-
-#[cfg(not(feature = "nightly"))]
-struct AssertRecoverSafe<T>(T);
-
-#[cfg(not(feature = "nightly"))]
-impl<T> AssertRecoverSafe<T> {
-    fn new(t: T) -> Self {
-        AssertRecoverSafe(t)
-    }
-}
-
-#[cfg(not(feature = "nightly"))]
-impl<T> ::std::ops::Deref for AssertRecoverSafe<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
-
-#[cfg(not(feature = "nightly"))]
-impl<T> ::std::ops::DerefMut for AssertRecoverSafe<T> {
-    fn deref_mut(&mut self) -> &mut T {
-        &mut self.0
-    }
 }
 
 unsafe extern "C" fn bwrite<S: Write>(bio: *mut BIO, buf: *const c_char, len: c_int) -> c_int {
@@ -118,10 +89,7 @@ unsafe extern "C" fn bwrite<S: Write>(bio: *mut BIO, buf: *const c_char, len: c_
     let state = state::<S>(bio);
     let buf = slice::from_raw_parts(buf as *const _, len as usize);
 
-    let result = {
-        let mut youre_not_my_supervisor = AssertRecoverSafe::new(&mut *state);
-        recover(move || youre_not_my_supervisor.stream.write(buf))
-    };
+    let result = recover(|| state.stream.write(buf));
 
     match result {
         Ok(Ok(len)) => len as c_int,
@@ -145,11 +113,7 @@ unsafe extern "C" fn bread<S: Read>(bio: *mut BIO, buf: *mut c_char, len: c_int)
     let state = state::<S>(bio);
     let buf = slice::from_raw_parts_mut(buf as *mut _, len as usize);
 
-    let result = {
-        let mut youre_not_my_supervisor = AssertRecoverSafe::new(&mut *state);
-        let mut fuuuu = AssertRecoverSafe::new(buf);
-        recover(move || youre_not_my_supervisor.stream.read(&mut *fuuuu))
-    };
+    let result = recover(|| state.stream.read(buf));
 
     match result {
         Ok(Ok(len)) => len as c_int,
@@ -185,10 +149,7 @@ unsafe extern "C" fn ctrl<S: Write>(bio: *mut BIO,
                                     -> c_long {
     if cmd == BIO_CTRL_FLUSH {
         let state = state::<S>(bio);
-        let result = {
-            let mut youre_not_my_supervisor = AssertRecoverSafe::new(&mut *state);
-            recover(move || youre_not_my_supervisor.stream.flush())
-        };
+        let result = recover(|| state.stream.flush());
 
         match result {
             Ok(Ok(())) => 1,
