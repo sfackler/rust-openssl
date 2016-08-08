4d3c6868e7a895b45ff6e8f97bd5c5ba026e84f9
Steven Fackler <sfackler@gmail.com>
2016-08-07 20:57:44 -0700

    pkcs5 reform

--- openssl/src/crypto/pkcs5.rs
+++ openssl/src/crypto/pkcs5.rs
@@ -30,9 +30,7 @@ pub fn evp_bytes_to_key_pbkdf1_compatible(typ: symm::Type,
                                           salt: Option<&[u8]>,
                                           count: u32)
                                           -> Result<KeyIvPair, ErrorStack> {
-
     unsafe {
-
         let salt_ptr = match salt {
             Some(salt) => {
                 assert_eq!(salt.len(), ffi::PKCS5_SALT_LEN as usize);
@@ -76,77 +74,47 @@ pub fn evp_bytes_to_key_pbkdf1_compatible(typ: symm::Type,
 }
 
 /// Derives a key from a password and salt using the PBKDF2-HMAC-SHA1 algorithm.
-pub fn pbkdf2_hmac_sha1(pass: &str, salt: &[u8], iter: usize, keylen: usize) -> Vec<u8> {
+pub fn pbkdf2_hmac_sha1(pass: &[u8],
+                        salt: &[u8],
+                        iter: usize,
+                        keylen: usize)
+                        -> Result<Vec<u8>, ErrorStack> {
     unsafe {
-        assert!(iter >= 1);
-        assert!(keylen >= 1);
-
-        let mut out = Vec::with_capacity(keylen);
+        let mut out = vec![0; keylen];
 
         ffi::init();
 
-        let r = ffi::PKCS5_PBKDF2_HMAC_SHA1(pass.as_ptr(),
-                                            pass.len() as c_int,
-                                            salt.as_ptr(),
-                                            salt.len() as c_int,
-                                            iter as c_int,
-                                            keylen as c_int,
-                                            out.as_mut_ptr());
-
-        if r != 1 {
-            panic!();
-        }
-
-        out.set_len(keylen);
-
-        out
+        try_ssl!(ffi::PKCS5_PBKDF2_HMAC_SHA1(pass.as_ptr(),
+                                             pass.len() as c_int,
+                                             salt.as_ptr(),
+                                             salt.len() as c_int,
+                                             iter as c_int,
+                                             keylen as c_int,
+                                             out.as_mut_ptr()));
+        Ok(out)
     }
-}
-
-/// Derives a key from a password and salt using the PBKDF2-HMAC-SHA256 algorithm.
-#[cfg(feature = "pkcs5_pbkdf2_hmac")]
-pub fn pbkdf2_hmac_sha256(pass: &str, salt: &[u8], iter: usize, keylen: usize) -> Vec<u8> {
-    pbkdf2_hmac_sha(pass, salt, iter, unsafe { ffi::EVP_sha256() }, keylen)
-}
-
-/// Derives a key from a password and salt using the PBKDF2-HMAC-SHA512 algorithm.
-#[cfg(feature = "pkcs5_pbkdf2_hmac")]
-pub fn pbkdf2_hmac_sha512(pass: &str, salt: &[u8], iter: usize, keylen: usize) -> Vec<u8> {
-    pbkdf2_hmac_sha(pass, salt, iter, unsafe { ffi::EVP_sha512() }, keylen)
 }
 
 /// Derives a key from a password and salt using the PBKDF2-HMAC algorithm with a digest function.
 #[cfg(feature = "pkcs5_pbkdf2_hmac")]
-fn pbkdf2_hmac_sha(pass: &str,
+pub fn pbkdf2_hmac(pass: &[u8],
                    salt: &[u8],
                    iter: usize,
-                   digest: *const ffi::EVP_MD,
+                   hash: hash::Type,
                    keylen: usize)
-                   -> Vec<u8> {
+                   -> Result<Vec<u8>, ErrorStack> {
     unsafe {
-        assert!(iter >= 1);
-        assert!(keylen >= 1);
-
-        let mut out = Vec::with_capacity(keylen);
-
+        let mut out = vec![0; keylen];
         ffi::init();
-
-        let r = ffi::PKCS5_PBKDF2_HMAC(pass.as_ptr(),
-                                       pass.len() as c_int,
-                                       salt.as_ptr(),
-                                       salt.len() as c_int,
-                                       iter as c_int,
-                                       digest,
-                                       keylen as c_int,
-                                       out.as_mut_ptr());
-
-        if r != 1 {
-            panic!();
-        }
-
-        out.set_len(keylen);
-
-        out
+        try_ssl!(ffi::PKCS5_PBKDF2_HMAC(pass.as_ptr(),
+                                        pass.len() as c_int,
+                                        salt.as_ptr(),
+                                        salt.len() as c_int,
+                                        iter as c_int,
+                                        hash.evp_md(),
+                                        keylen as c_int,
+                                        out.as_mut_ptr()));
+        Ok(out)
     }
 }
 
@@ -159,36 +127,36 @@ mod tests {
     // http://tools.ietf.org/html/draft-josefsson-pbkdf2-test-vectors-06
     #[test]
     fn test_pbkdf2_hmac_sha1() {
-        assert_eq!(super::pbkdf2_hmac_sha1("password", "salt".as_bytes(), 1, 20),
+        assert_eq!(super::pbkdf2_hmac_sha1(b"password", b"salt", 1, 20).unwrap(),
                    vec![0x0c_u8, 0x60_u8, 0xc8_u8, 0x0f_u8, 0x96_u8, 0x1f_u8, 0x0e_u8, 0x71_u8,
                         0xf3_u8, 0xa9_u8, 0xb5_u8, 0x24_u8, 0xaf_u8, 0x60_u8, 0x12_u8, 0x06_u8,
                         0x2f_u8, 0xe0_u8, 0x37_u8, 0xa6_u8]);
 
-        assert_eq!(super::pbkdf2_hmac_sha1("password", "salt".as_bytes(), 2, 20),
+        assert_eq!(super::pbkdf2_hmac_sha1(b"password", b"salt", 2, 20).unwrap(),
                    vec![0xea_u8, 0x6c_u8, 0x01_u8, 0x4d_u8, 0xc7_u8, 0x2d_u8, 0x6f_u8, 0x8c_u8,
                         0xcd_u8, 0x1e_u8, 0xd9_u8, 0x2a_u8, 0xce_u8, 0x1d_u8, 0x41_u8, 0xf0_u8,
                         0xd8_u8, 0xde_u8, 0x89_u8, 0x57_u8]);
 
-        assert_eq!(super::pbkdf2_hmac_sha1("password", "salt".as_bytes(), 4096, 20),
+        assert_eq!(super::pbkdf2_hmac_sha1(b"password", b"salt", 4096, 20).unwrap(),
                    vec![0x4b_u8, 0x00_u8, 0x79_u8, 0x01_u8, 0xb7_u8, 0x65_u8, 0x48_u8, 0x9a_u8,
                         0xbe_u8, 0xad_u8, 0x49_u8, 0xd9_u8, 0x26_u8, 0xf7_u8, 0x21_u8, 0xd0_u8,
                         0x65_u8, 0xa4_u8, 0x29_u8, 0xc1_u8]);
 
-        assert_eq!(super::pbkdf2_hmac_sha1("password", "salt".as_bytes(), 16777216, 20),
+        assert_eq!(super::pbkdf2_hmac_sha1(b"password", b"salt", 16777216, 20).unwrap(),
                    vec![0xee_u8, 0xfe_u8, 0x3d_u8, 0x61_u8, 0xcd_u8, 0x4d_u8, 0xa4_u8, 0xe4_u8,
                         0xe9_u8, 0x94_u8, 0x5b_u8, 0x3d_u8, 0x6b_u8, 0xa2_u8, 0x15_u8, 0x8c_u8,
                         0x26_u8, 0x34_u8, 0xe9_u8, 0x84_u8]);
 
-        assert_eq!(super::pbkdf2_hmac_sha1("passwordPASSWORDpassword",
-                                           "saltSALTsaltSALTsaltSALTsaltSALTsalt".as_bytes(),
+        assert_eq!(super::pbkdf2_hmac_sha1(b"passwordPASSWORDpassword",
+                                           b"saltSALTsaltSALTsaltSALTsaltSALTsalt",
                                            4096,
-                                           25),
+                                           25).unwrap(),
                    vec![0x3d_u8, 0x2e_u8, 0xec_u8, 0x4f_u8, 0xe4_u8, 0x1c_u8, 0x84_u8, 0x9b_u8,
                         0x80_u8, 0xc8_u8, 0xd8_u8, 0x36_u8, 0x62_u8, 0xc0_u8, 0xe4_u8, 0x4a_u8,
                         0x8b_u8, 0x29_u8, 0x1a_u8, 0x96_u8, 0x4c_u8, 0xf2_u8, 0xf0_u8, 0x70_u8,
                         0x38_u8]);
 
-        assert_eq!(super::pbkdf2_hmac_sha1("pass\x00word", "sa\x00lt".as_bytes(), 4096, 16),
+        assert_eq!(super::pbkdf2_hmac_sha1(b"pass\x00word", b"sa\x00lt", 4096, 16).unwrap(),
                    vec![0x56_u8, 0xfa_u8, 0x6a_u8, 0xa7_u8, 0x55_u8, 0x48_u8, 0x09_u8, 0x9d_u8,
                         0xcc_u8, 0x37_u8, 0xd7_u8, 0xf0_u8, 0x34_u8, 0x25_u8, 0xe0_u8, 0xc3_u8]);
     }
@@ -198,11 +166,11 @@ mod tests {
     #[test]
     #[cfg(feature = "pkcs5_pbkdf2_hmac")]
     fn test_pbkdf2_hmac_sha256() {
-        assert_eq!(super::pbkdf2_hmac_sha256("passwd", "salt".as_bytes(), 1, 16),
+        assert_eq!(super::pbkdf2_hmac(b"passwd", b"salt", 1, hash::Type::SHA256, 16).unwrap(),
                    vec![0x55_u8, 0xac_u8, 0x04_u8, 0x6e_u8, 0x56_u8, 0xe3_u8, 0x08_u8, 0x9f_u8,
                         0xec_u8, 0x16_u8, 0x91_u8, 0xc2_u8, 0x25_u8, 0x44_u8, 0xb6_u8, 0x05_u8]);
 
-        assert_eq!(super::pbkdf2_hmac_sha256("Password", "NaCl".as_bytes(), 80000, 16),
+        assert_eq!(super::pbkdf2_hmac(b"Password", b"NaCl", 80000, hash::Type::SHA256, 16).unwrap(),
                    vec![0x4d_u8, 0xdc_u8, 0xd8_u8, 0xf6_u8, 0x0b_u8, 0x98_u8, 0xbe_u8, 0x21_u8,
                         0x83_u8, 0x0c_u8, 0xee_u8, 0x5e_u8, 0xf2_u8, 0x27_u8, 0x01_u8, 0xf9_u8]);
     }
@@ -212,7 +180,7 @@ mod tests {
     #[test]
     #[cfg(feature = "pkcs5_pbkdf2_hmac")]
     fn test_pbkdf2_hmac_sha512() {
-        assert_eq!(super::pbkdf2_hmac_sha512("password", "NaCL".as_bytes(), 1, 64),
+        assert_eq!(super::pbkdf2_hmac(b"password", b"NaCL", 1, hash::Type::SHA512, 64).unwrap(),
                    vec![0x73_u8, 0xde_u8, 0xcf_u8, 0xa5_u8, 0x8a_u8, 0xa2_u8, 0xe8_u8, 0x4f_u8,
                         0x94_u8, 0x77_u8, 0x1a_u8, 0x75_u8, 0x73_u8, 0x6b_u8, 0xb8_u8, 0x8b_u8,
                         0xd3_u8, 0xc7_u8, 0xb3_u8, 0x82_u8, 0x70_u8, 0xcf_u8, 0xb5_u8, 0x0c_u8,
@@ -222,7 +190,7 @@ mod tests {
                         0x60_u8, 0x60_u8, 0xa0_u8, 0x9f_u8, 0x76_u8, 0x41_u8, 0x5e_u8, 0x9f_u8,
                         0x71_u8, 0xea_u8, 0x47_u8, 0xf9_u8, 0xe9_u8, 0x06_u8, 0x43_u8, 0x06_u8]);
 
-        assert_eq!(super::pbkdf2_hmac_sha512("pass\0word", "sa\0lt".as_bytes(), 1, 64),
+        assert_eq!(super::pbkdf2_hmac(b"pass\0word", b"sa\0lt", 1, hash::Type::SHA512, 64).unwrap(),
                    vec![0x71_u8, 0xa0_u8, 0xec_u8, 0x84_u8, 0x2a_u8, 0xbd_u8, 0x5c_u8, 0x67_u8,
                         0x8b_u8, 0xcf_u8, 0xd1_u8, 0x45_u8, 0xf0_u8, 0x9d_u8, 0x83_u8, 0x52_u8,
                         0x2f_u8, 0x93_u8, 0x36_u8, 0x15_u8, 0x60_u8, 0x56_u8, 0x3c_u8, 0x4d_u8,
@@ -232,10 +200,11 @@ mod tests {
                         0xb2_u8, 0x0f_u8, 0x06_u8, 0xbc_u8, 0x53_u8, 0x5e_u8, 0x5a_u8, 0xb5_u8,
                         0x44_u8, 0x0d_u8, 0xf7_u8, 0xe8_u8, 0x78_u8, 0x29_u8, 0x6f_u8, 0xa7_u8]);
 
-        assert_eq!(super::pbkdf2_hmac_sha512("passwordPASSWORDpassword",
-                                             "salt\0\0\0".as_bytes(),
-                                             50,
-                                             64),
+        assert_eq!(super::pbkdf2_hmac(b"passwordPASSWORDpassword",
+                                      b"salt\0\0\0",
+                                      50,
+                                      hash::Type::SHA512,
+                                      64).unwrap(),
                    vec![0x01_u8, 0x68_u8, 0x71_u8, 0xa4_u8, 0xc4_u8, 0xb7_u8, 0x5f_u8, 0x96_u8,
                         0x85_u8, 0x7f_u8, 0xd2_u8, 0xb9_u8, 0xf8_u8, 0xca_u8, 0x28_u8, 0x02_u8,
                         0x3b_u8, 0x30_u8, 0xee_u8, 0x2a_u8, 0x39_u8, 0xf5_u8, 0xad_u8, 0xca_u8,
