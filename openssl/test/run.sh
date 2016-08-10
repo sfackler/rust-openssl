00db0bc4b312218b4f4165909a8bbf56522b0099
Steven Fackler <sfackler@gmail.com>
2016-08-09 22:56:08 -0700

    Test hmac features

--- openssl/test/run.sh
+++ openssl/test/run.sh
@@ -4,7 +4,7 @@ set -e
 MAIN_TARGETS=https://static.rust-lang.org/dist
 
 if [ "$TEST_FEATURES" == "true" ]; then
-    FEATURES="tlsv1_2 tlsv1_1 dtlsv1 dtlsv1_2 sslv3 aes_xts aes_ctr npn alpn rfc5114 ecdh_auto pkcs5_pbkdf2_hmac x509_clone ssl_context_clone x509_generator_request"
+    FEATURES="tlsv1_2 tlsv1_1 dtlsv1 dtlsv1_2 sslv3 aes_xts aes_ctr npn alpn rfc5114 ecdh_auto pkcs5_pbkdf2_hmac x509_clone ssl_context_clone x509_generator_request hmac hmac_clone"
 fi
 
 if [ "$TRAVIS_OS_NAME" != "osx" ]; then
