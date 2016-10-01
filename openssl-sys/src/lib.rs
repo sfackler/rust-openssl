44ed665f02fea1cdd4c8fda81e20480383b1d3aa
manuels <manuel.schoelling@gmx.de>
2016-10-01 13:42:13 +0200

    Add RAND_status()
    RAND_status() returns 1 if the PRNG has been seeded with enough data, 0 otherwise.

--- openssl-sys/src/lib.rs
+++ openssl-sys/src/lib.rs
@@ -873,6 +873,7 @@ extern "C" {
                              out: *mut u8) -> c_int;
 
     pub fn RAND_bytes(buf: *mut u8, num: c_int) -> c_int;
+    pub fn RAND_status() -> c_int;
 
     pub fn RSA_new() -> *mut RSA;
     pub fn RSA_free(rsa: *mut RSA);
