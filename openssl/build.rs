f520aa2860dcb8f995a8fa5bd602916f96de1d8a
Steven Fackler <sfackler@gmail.com>
2016-10-14 20:50:45 -0700

    Handle OPENSSL_NO_COMP
    Closes #459

--- openssl/build.rs
+++ openssl/build.rs
@@ -12,4 +12,9 @@ fn main() {
     if env::var("DEP_OPENSSL_IS_110").is_ok() {
         println!("cargo:rustc-cfg=ossl110");
     }
+    if let Ok(vars) = env::var("DEP_OPENSSL_OSSLCONF") {
+        for var in vars.split(",") {
+            println!("cargo:rustc-cfg=osslconf=\"{}\"", var);
+        }
+    }
 }
