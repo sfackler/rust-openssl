51a226eb4b4977ce26945cf608d1be34b10d6286
Steven Fackler <sfackler@gmail.com>
2017-07-04 20:57:00 -0700

    Support PKCS#1 RSA public keys
    Closes #656

--- openssl-sys/src/lib.rs
+++ openssl-sys/src/lib.rs
@@ -2421,6 +2421,8 @@ extern "C" {
 
     pub fn i2d_RSA_PUBKEY(k: *mut RSA, buf: *mut *mut u8) -> c_int;
     pub fn d2i_RSA_PUBKEY(k: *mut *mut RSA, buf: *mut *const u8, len: c_long) -> *mut RSA;
+    pub fn i2d_RSAPublicKey(k: *const RSA, buf: *mut *mut u8) -> c_int;
+    pub fn d2i_RSAPublicKey(k: *mut *mut RSA, buf: *mut *const u8, len: c_long) -> *mut RSA;
     pub fn i2d_RSAPrivateKey(k: *const RSA, buf: *mut *mut u8) -> c_int;
     pub fn d2i_RSAPrivateKey(k: *mut *mut RSA, buf: *mut *const u8, len: c_long) -> *mut RSA;
 
