523ca9027ea5e3ab74bed884d660d6316c0397c1
Matthew Taylor <mstaveleytaylor@gmail.com>
2017-07-10 18:09:55 +0100

    Add error reason and function codes to openssl-sys

--- openssl-sys/src/lib.rs
+++ openssl-sys/src/lib.rs
@@ -8,6 +8,8 @@ use libc::{c_void, c_int, c_char, c_ulong, c_long, c_uint, c_uchar, size_t, FILE
 use std::ptr;
 use std::mem;
 
+include!("errors.rs");
+
 #[cfg(any(ossl101, ossl102))]
 mod ossl10x;
 #[cfg(any(ossl101, ossl102))]
@@ -175,7 +177,6 @@ pub const ERR_TXT_MALLOCED: c_int = 0x01;
 pub const ERR_TXT_STRING: c_int = 0x02;
 
 pub const ERR_LIB_PEM: c_int = 9;
-pub const PEM_R_NO_START_LINE: c_int = 108;
 
 pub const EVP_MAX_MD_SIZE: c_uint = 64;
 pub const EVP_PKEY_RSA: c_int = NID_rsaEncryption;
