220c707fd95de3f3230365a8ee9dd8789a1ee421
johnthagen <johnthagen@gmail.com>
2017-09-28 09:49:03 -0400

    Document rand module

--- openssl/src/rand.rs
+++ openssl/src/rand.rs
@@ -1,9 +1,27 @@
+//! Cryptographically strong random bytes.
+//!
+//! This module exposes functionality to put cryptographically strong
+//! pseudo-random bytes into a buffer.
+//!
+//! # Examples
+//!
+//! To generate a buffer with cryptographically strong bytes:
+//!
+//! ```
+//! let muf buf = [0; 256]
+//! rand_bytes(&mut buf).unwrap();
+//! ```
+//!
+//! # External OpenSSL Documentation
+//!
+//! [RAND_bytes](https://www.openssl.org/docs/man1.1.0/crypto/RAND_bytes.html)
 use libc::c_int;
 use ffi;
 
 use cvt;
 use error::ErrorStack;
 
+/// Fills buffer with cryptographically strong pseudo-random bytes.
 pub fn rand_bytes(buf: &mut [u8]) -> Result<(), ErrorStack> {
     unsafe {
         ffi::init();
