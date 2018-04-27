03a4c6bd26f70f37c79e594a86a0388b880ca998
Steven Fackler <sfackler@gmail.com>
2018-04-26 22:45:09 -0700

    Reform version checking logic
    Rather than having an infinitely growing set of things to look for, just
    grab the literal version out. We also provide that to downstream crates,
    and it should be used rather than the random assortment of other stuff
    that's also passed down.

--- openssl-sys/src/lib.rs
+++ openssl-sys/src/lib.rs
@@ -5,8 +5,8 @@
 extern crate libc;
 
 use libc::{c_char, c_int, c_long, c_uchar, c_uint, c_ulong, c_void, size_t, FILE};
-use std::ptr;
 use std::mem;
+use std::ptr;
 
 #[cfg(any(ossl101, ossl102))]
 mod ossl10x;
@@ -1277,14 +1277,14 @@ pub const SSL_VERIFY_NONE: c_int = 0;
 pub const SSL_VERIFY_PEER: c_int = 1;
 pub const SSL_VERIFY_FAIL_IF_NO_PEER_CERT: c_int = 2;
 
-#[cfg(not(any(libressl261, libressl262, libressl26x, libressl27, ossl101)))]
+#[cfg(not(any(libressl261, libressl262, libressl26x, libressl27x, ossl101)))]
 pub const SSL_OP_TLSEXT_PADDING: c_ulong = 0x00000010;
-#[cfg(any(libressl261, libressl262, libressl26x, libressl27))]
+#[cfg(any(libressl261, libressl262, libressl26x, libressl27x))]
 pub const SSL_OP_TLSEXT_PADDING: c_ulong = 0x0;
 pub const SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS: c_ulong = 0x00000800;
-#[cfg(not(any(libressl261, libressl262, libressl26x, libressl27)))]
+#[cfg(not(any(libressl261, libressl262, libressl26x, libressl27x)))]
 pub const SSL_OP_CRYPTOPRO_TLSEXT_BUG: c_ulong = 0x80000000;
-#[cfg(any(libressl261, libressl262, libressl26x, libressl27))]
+#[cfg(any(libressl261, libressl262, libressl26x, libressl27x))]
 pub const SSL_OP_CRYPTOPRO_TLSEXT_BUG: c_ulong = 0x0;
 pub const SSL_OP_LEGACY_SERVER_CONNECT: c_ulong = 0x00000004;
 #[cfg(not(libressl))]
