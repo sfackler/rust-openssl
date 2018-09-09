93a4e96255744363b80603e821e94b551621fbd8
Steven Fackler <sfackler@gmail.com>
2018-09-08 21:13:15 -0700

    Refactor openssl-sys
    The old layout tried to structure itself by version but it ended up with
    a lot of duplication. Instead, follow the structure of the header files.

---
+++ openssl-sys/src/object.rs
@@ -0,0 +1,18 @@
+use libc::*;
+
+use *;
+
+extern "C" {
+    pub fn OBJ_nid2ln(nid: c_int) -> *const c_char;
+    pub fn OBJ_nid2sn(nid: c_int) -> *const c_char;
+    pub fn OBJ_obj2nid(o: *const ASN1_OBJECT) -> c_int;
+    pub fn OBJ_obj2txt(
+        buf: *mut c_char,
+        buf_len: c_int,
+        a: *const ASN1_OBJECT,
+        no_name: c_int,
+    ) -> c_int;
+
+    pub fn OBJ_find_sigid_algs(signid: c_int, pdig_nid: *mut c_int, ppkey_nid: *mut c_int)
+        -> c_int;
+}
