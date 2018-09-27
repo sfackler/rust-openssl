b6971883bea0ba9701e47570418f3289d64a65f0
Charlie Li <git@vishwin.info>
2018-09-27 01:19:39 -0400

    Support the rest of LibreSSL 2.8.x
    LibreSSL 2.8.1 released, so update the check for all versions in the series, not just 2.8.0.

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -497,7 +497,7 @@ See rust-openssl README for more information:
             (6, 2) => ('6', '2'),
             (6, _) => ('6', 'x'),
             (7, _) => ('7', 'x'),
-            (8, 0) => ('8', 'x'),
+            (8, _) => ('8', 'x'),
             _ => version_error(),
         };
 
@@ -538,7 +538,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
-through 2.7, but a different version of OpenSSL was found. The build is now aborting
+through 2.8, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
