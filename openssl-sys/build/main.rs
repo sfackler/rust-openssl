af4488357c9b3e003b883e89c16aaa675ad0c6ac
Laurence Tratt <laurie@tratt.net>
2018-12-03 17:33:53 +0000

    Explicitly support LibreSSL 2.9.0.

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -504,6 +504,7 @@ See rust-openssl README for more information:
             (8, 0) => ('8', '0'),
             (8, 1) => ('8', '1'),
             (8, _) => ('8', 'x'),
+            (9, 0) => ('9', '0'),
             _ => version_error(),
         };
 
@@ -544,7 +545,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
-through 2.8.1, but a different version of OpenSSL was found. The build is now aborting
+through 2.9.0, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
