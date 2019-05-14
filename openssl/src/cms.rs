3dbfef06c5481b9bae815bf9b829660b95caee50
Alexis Mousset <alexis.mousset@rudder.io>
2019-05-14 22:51:12 +0200

    Put Recipient Info in a separate mod

--- openssl/src/cms.rs
+++ openssl/src/cms.rs
@@ -1,22 +1,19 @@
 //! SMIME implementation using CMS
 //!
-//! CMS (PKCS#7) is an encyption standard.  It allows signing and ecrypting data using
+//! CMS (PKCS#7) is an encryption standard.  It allows signing and encrypting data using
 //! X.509 certificates.  The OpenSSL implementation of CMS is used in email encryption
 //! generated from a `Vec` of bytes.  This `Vec` follows the smime protocol standards.
 //! Data accepted by this module will be smime type `enveloped-data`.
 
 use ffi;
 use foreign_types::{ForeignType, ForeignTypeRef};
-#[cfg(ossl110)]
-use foreign_types::Opaque;
 use std::ptr;
 
-#[cfg(ossl110)]
-use asn1::Asn1IntegerRef;
 use bio::{MemBio, MemBioSlice};
 use error::ErrorStack;
 use libc::c_uint;
 use pkey::{HasPrivate, PKeyRef};
+<<<<<<< HEAD
 #[cfg(ossl110)]
 use stack::{Stackable, StackRef};
 use symm::Cipher;
@@ -26,6 +23,9 @@ use x509::X509NameRef;
 use x509::store::{X509StoreRef, X509StoreRef};
 use symm::Cipher;
 use {cvt, cvt_n, cvt_p};
+
+#[cfg(ossl110)]
+pub use self::recipient_info::*;
 
 bitflags! {
     pub struct CMSOptions : c_uint {
@@ -180,19 +180,6 @@ impl CmsContentInfoRef {
         }
     }
 
-    #[cfg(ossl110)]
-    /// Given that we're dealing with the `EnvelopedData`, returns the recipients of the message.
-    ///
-    /// OpenSSL documentation at [`CMS_get0_RecipientInfos`]
-    ///
-    /// [`CMS_get0_RecipientInfos`]: https://www.openssl.org/docs/manmaster/man3/CMS_RecipientInfo_decrypt.html
-    pub fn get_recipient_infos(&self) -> Result<&StackRef<CmsRecipientInfo>, ErrorStack> {
-        unsafe {
-            let recipient_infos = cvt_p(ffi::CMS_get0_RecipientInfos(self.as_ptr()))?;
-            Ok(StackRef::from_ptr(recipient_infos))
-        }
-    }
-
     to_der! {
         /// Serializes this CmsContentInfo using DER.
         ///
@@ -320,79 +307,149 @@ impl CmsContentInfo {
 }
 
 #[cfg(ossl110)]
-/// An owned version of [`CmsRecipientInfoRef`]
-///
-/// [`CmsRecipientInfoRef`]:struct.CmsRecipientInfoRef.html
-pub struct CmsRecipientInfo(*mut ffi::CMS_RecipientInfo);
+mod recipient_info {
+    use asn1::Asn1IntegerRef;
+    use stack::Stackable;
+    use x509::X509NameRef;
+    use foreign_types::Opaque;
+    use super::*;
 
-#[cfg(ossl110)]
-impl ForeignType for CmsRecipientInfo {
-    type CType = ffi::CMS_RecipientInfo;
-    type Ref = CmsRecipientInfoRef;
-
-    #[inline]
-    unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
-        Self(ptr)
+    impl CmsContentInfoRef {
+        /// Given that we're dealing with the `EnvelopedData`, returns the recipients of the message.
+        ///
+        /// OpenSSL documentation at [`CMS_get0_RecipientInfos`]
+        ///
+        /// [`CMS_get0_RecipientInfos`]: https://www.openssl.org/docs/manmaster/man3/CMS_RecipientInfo_decrypt.html
+        pub fn get_recipient_infos(&self) -> Result<&StackRef<CmsRecipientInfo>, ErrorStack> {
+            unsafe {
+                let recipient_infos = cvt_p(ffi::CMS_get0_RecipientInfos(self.as_ptr()))?;
+                Ok(StackRef::from_ptr(recipient_infos))
+            }
+        }
     }
 
-    #[inline]
-    fn as_ptr(&self) -> *mut Self::CType {
-        self.0
-    }
-}
-
-#[cfg(ossl110)]
-/// Reference to a [`CMS_RecipientInfo`].
-///
-/// [`CMS_RecipientInfo`]: https://www.openssl.org/docs/manmaster/man3/CMS_RecipientInfo_decrypt.html
-pub struct CmsRecipientInfoRef(Opaque);
-
-#[cfg(ossl110)]
-impl ForeignTypeRef for CmsRecipientInfoRef {
-    type CType = ffi::CMS_RecipientInfo;
-}
-
-#[cfg(ossl110)]
-impl Stackable for CmsRecipientInfo {
-    type StackType = ffi::stack_st_CMS_RecipientInfo;
-}
-
-#[cfg(ossl110)]
-/// The bindings to the openssl's type of `CMS_RecipientInfo` which can be extracted with
-/// openssl's `CMS_RecipientInfo_ktri_get0_singer_id()` function.
-pub enum RecipientInfo<'cms> {
-    Identifier,
-    Info {
-        issuer: &'cms X509NameRef,
-        serial_number: &'cms Asn1IntegerRef,
-    },
-}
-
-#[cfg(ossl110)]
-impl CmsRecipientInfoRef {
-    /// Implementation of [`CMS_RecipientInfo_ktri_get0_signer_id`].
+    /// An owned version of [`CmsRecipientInfoRef`]
     ///
-    /// [`CMS_RecipientInfo_ktri_get0_signer_id`]: https://www.openssl.org/docs/manmaster/man3/CMS_RecipientInfo_decrypt.html
-    pub fn get_recipient_info<'cms>(&self) -> Result<RecipientInfo<'cms>, ErrorStack> {
-        unsafe {
-            let mut key_id = ptr::null_mut();
-            let mut issuer = ptr::null_mut();
-            let mut serial_number = ptr::null_mut();
-            cvt(ffi::CMS_RecipientInfo_ktri_get0_signer_id(
-                self.as_ptr(),
-                &mut key_id,
-                &mut issuer,
-                &mut serial_number,
-            ))?;
+    /// [`CmsRecipientInfoRef`]:struct.CmsRecipientInfoRef.html
+    pub struct CmsRecipientInfo(*mut ffi::CMS_RecipientInfo);
 
-            if !key_id.is_null() {
-                // TODO: Implement (ffi::ASN1_OCTET_STRING) bindings.
-                Ok(RecipientInfo::Identifier)
-            } else {
-                Ok(RecipientInfo::Info {
-                    issuer: X509NameRef::from_ptr(issuer),
-                    serial_number: Asn1IntegerRef::from_ptr(serial_number),
-                })
+
+    impl ForeignType for CmsRecipientInfo {
+        type CType = ffi::CMS_RecipientInfo;
+        type Ref = CmsRecipientInfoRef;
+
+        #[inline]
+        unsafe fn from_ptr(ptr: *mut Self::CType) -> Self {
+            Self(ptr)
+        }
+
+        #[inline]
+        fn as_ptr(&self) -> *mut Self::CType {
+            self.0
+        }
+    }
+
+    /// Reference to a [`CMS_RecipientInfo`].
+    ///
+    /// [`CMS_RecipientInfo`]: https://www.openssl.org/docs/manmaster/man3/CMS_RecipientInfo_decrypt.html
+    pub struct CmsRecipientInfoRef(Opaque);
+
+    impl ForeignTypeRef for CmsRecipientInfoRef {
+        type CType = ffi::CMS_RecipientInfo;
+    }
+
+    impl Stackable for CmsRecipientInfo {
+        type StackType = ffi::stack_st_CMS_RecipientInfo;
+    }
+
+    /// The bindings to the openssl's type of `CMS_RecipientInfo` which can be extracted with
+    /// openssl's `CMS_RecipientInfo_ktri_get0_singer_id()` function.
+    pub enum RecipientInfo<'cms> {
+        Identifier,
+        Info {
+            issuer: &'cms X509NameRef,
+            serial_number: &'cms Asn1IntegerRef,
+        },
+    }
+
+    impl CmsRecipientInfoRef {
+        /// Implementation of [`CMS_RecipientInfo_ktri_get0_signer_id`].
+        ///
+        /// [`CMS_RecipientInfo_ktri_get0_signer_id`]: https://www.openssl.org/docs/manmaster/man3/CMS_RecipientInfo_decrypt.html
+        pub fn get_recipient_info<'cms>(&self) -> Result<RecipientInfo<'cms>, ErrorStack> {
+            unsafe {
+                let mut key_id = ptr::null_mut();
+                let mut issuer = ptr::null_mut();
+                let mut serial_number = ptr::null_mut();
+                cvt(ffi::CMS_RecipientInfo_ktri_get0_signer_id(
+                    self.as_ptr(),
+                    &mut key_id,
+                    &mut issuer,
+                    &mut serial_number,
+                ))?;
+
+                if !key_id.is_null() {
+                    // TODO: Implement (ffi::ASN1_OCTET_STRING) bindings.
+                    Ok(RecipientInfo::Identifier)
+                } else {
+                    Ok(RecipientInfo::Info {
+                        issuer: X509NameRef::from_ptr(issuer),
+                        serial_number: Asn1IntegerRef::from_ptr(serial_number),
+                    })
+                }
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+        use stack::Stack;
+
+        #[test]
+        fn extract_recipient_info() {
+            // load cert with public key only
+            let pub_cert_bytes = include_bytes!("../test/cms_pubkey.der");
+            let pub_cert = X509::from_der(pub_cert_bytes).expect("failed to load pub cert");
+
+            // encrypt cms message using public key cert
+            let input = String::from("My Message");
+            let mut cert_stack = Stack::new().expect("failed to create stack");
+            cert_stack.push(pub_cert.clone()).expect("failed to add pub cert to stack");
+
+            let encrypt = CmsContentInfo::encrypt(&cert_stack, &input.as_bytes(), Cipher::des_ede3_cbc(), CMSOptions::empty())
+                .expect("failed create encrypted cms");
+            let encrypt = encrypt.to_der().expect("failed to create der from cms");
+
+            // decrypt cms message using private key cert
+            let enveloped_data = CmsContentInfo::from_der(&encrypt).expect("failed read cms from der");
+
+            // extract the recipient's information
+            let recipients = enveloped_data.get_recipient_infos().expect("failed to get recipient infos");
+            assert_eq!(recipients.len(), 1);
+
+            let recp = recipients.get(0).unwrap().get_recipient_info().expect("failed to get recipient info");
+            match recp {
+                RecipientInfo::Identifier => {
+                panic!("wrong information inside the recipient data");
+                }
+                RecipientInfo::Info { issuer, serial_number } => {
+                    // compare the serial numbers
+                    let pub_cert_serial_number = pub_cert.serial_number()
+                        .to_bn()
+                        .expect("could not convert serial number to bn")
+                        .to_string();
+                    let recp_serial_number = serial_number
+                        .to_bn()
+                        .expect("could not convert recipient's serial number to bn")
+                        .to_string();
+                    assert_eq!(pub_cert_serial_number, recp_serial_number);
+
+                    // compare issuers
+                    for (cert, expected) in pub_cert.issuer_name().entries().zip(issuer.entries()) {
+                        assert_eq!(cert.data().as_slice(), expected.data().as_slice());
+                    }
+                }
             }
         }
     }
@@ -516,58 +573,5 @@ mod test {
 
         let verify = verify.verify_detached(Some(&cert_stack), &store, input.as_bytes(), CMSOptions::empty()).expect("failed to verify cms");
         assert!(verify);
-    }
-
-    #[test]
-    fn extract_recipient_info() {
-        // load cert with public key only
-        let pub_cert_bytes = include_bytes!("../test/cms_pubkey.der");
-        let pub_cert = X509::from_der(pub_cert_bytes).expect("failed to load pub cert");
-
-        // encrypt cms message using public key cert
-        let input = String::from("My Message");
-        let mut cert_stack = Stack::new().expect("failed to create stack");
-        cert_stack.push(pub_cert.clone()).expect("failed to add pub cert to stack");
-
-        let encrypt = CmsContentInfo::encrypt(&cert_stack, &input.as_bytes(), Cipher::des_ede3_cbc(), CMSOptions::empty())
-            .expect("failed create encrypted cms");
-        let encrypt = encrypt.to_der().expect("failed to create der from cms");
-
-        // decrypt cms message using private key cert
-        #[cfg(not(ossl110))]
-        let _ = CmsContentInfo::from_der(&encrypt).expect("failed read cms from der");
-
-        #[cfg(ossl110)] 
-        {
-            let enveloped_data = CmsContentInfo::from_der(&encrypt).expect("failed read cms from der");
-
-            // extract the recipient's information
-            let recipients = enveloped_data.get_recipient_infos().expect("failed to get recipient infos");
-            assert_eq!(recipients.len(), 1);
-
-            let recp = recipients.get(0).unwrap().get_recipient_info().expect("failed to get recipient info");
-            match recp {
-                RecipientInfo::Identifier => {
-                    panic!("wrong information inside the recipient data");
-                }
-                RecipientInfo::Info { issuer, serial_number } => {
-                    // compare the serial numbers
-                    let pub_cert_serial_number = pub_cert.serial_number()
-                        .to_bn()
-                        .expect("could not convert serial number to bn")
-                        .to_string();
-                    let recp_serial_number = serial_number
-                        .to_bn()
-                        .expect("could not convert recipient's serial number to bn")
-                        .to_string();
-                    assert_eq!(pub_cert_serial_number, recp_serial_number);
-
-                    // compare issuers
-                    for (cert, expected) in pub_cert.issuer_name().entries().zip(issuer.entries()) {
-                        assert_eq!(cert.data().as_slice(), expected.data().as_slice());
-                    }
-                }
-            }
-        }
     }
 }
