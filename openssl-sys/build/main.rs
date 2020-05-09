54fbab73b7a6b334e727511416b677b14dbbb8b7
Charlie Li <git@vishwin.info>
2020-05-08 21:21:51 -0400

    LibreSSL 3.1 branch marked as stable; add support

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -217,6 +217,7 @@ See rust-openssl README for more information:
             (3, 0, 1) => ('3', '0', '1'),
             (3, 0, _) => ('3', '0', 'x'),
             (3, 1, 0) => ('3', '1', '0'),
+            (3, 1, _) => ('3', '1', 'x'),
             _ => version_error(),
         };
 
@@ -257,7 +258,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
-through 3.0.x, but a different version of OpenSSL was found. The build is now aborting
+through 3.1.x, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
