32de038385b6324894514c4eff15cb9aa88d8d4f
Charlie Li <git@vishwin.info>
2020-06-02 06:19:00 -0400

    Support LibreSSL 3.2.0

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -220,6 +220,7 @@ See rust-openssl README for more information:
             (3, 0, _) => ('3', '0', 'x'),
             (3, 1, 0) => ('3', '1', '0'),
             (3, 1, _) => ('3', '1', 'x'),
+            (3, 2, 0) => ('3', '2', '0'),
             _ => version_error(),
         };
 
@@ -260,7 +261,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
-through 3.1.x, but a different version of OpenSSL was found. The build is now aborting
+through 3.2.0, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
