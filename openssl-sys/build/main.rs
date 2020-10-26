a7ea9a7517ea8b96681c7f65e21c1c4322df151a
Brennan Vincent <brennan@umanwizard.com>
2020-10-26 19:33:46 -0400

    Fix build on OpenBSD 6.8

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -221,6 +221,8 @@ See rust-openssl README for more information:
             (3, 1, 0) => ('3', '1', '0'),
             (3, 1, _) => ('3', '1', 'x'),
             (3, 2, 0) => ('3', '2', '0'),
+            (3, 2, 1) => ('3', '2', '1'),
+            (3, 2, 2) => ('3', '2', '2'),
             _ => version_error(),
         };
 
@@ -261,7 +263,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
-through 3.2.0, but a different version of OpenSSL was found. The build is now aborting
+through 3.2.2, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
