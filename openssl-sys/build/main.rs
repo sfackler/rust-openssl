fe94a15a48f8f011ea8f9e66df56bf9ee077f6e1
Brennan Vincent <brennan@umanwizard.com>
2020-10-26 18:41:44 -0500

    Update main.rs

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -222,7 +222,7 @@ See rust-openssl README for more information:
             (3, 1, _) => ('3', '1', 'x'),
             (3, 2, 0) => ('3', '2', '0'),
             (3, 2, 1) => ('3', '2', '1'),
-            (3, 2, 2) => ('3', '2', '2'),
+            (3, 2, _) => ('3', '2', 'x'),
             _ => version_error(),
         };
 
@@ -263,7 +263,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
-through 3.2.2, but a different version of OpenSSL was found. The build is now aborting
+through 3.2.x, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
