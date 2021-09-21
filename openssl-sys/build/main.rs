9fc64b4357f39c5f8da2bff3d19a974ce934f794
Henrik Böving <hargonix@gmail.com>
2021-09-21 12:41:06 +0200

    Address review

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -234,7 +234,6 @@ See rust-openssl README for more information:
             (3, 3, 1) => ('3', '3', '1'),
             (3, 3, _) => ('3', '3', 'x'),
             (3, 4, 0) => ('3', '4', '0'),
-            (3, 4, _) => ('3', '4', 'x'),
             _ => version_error(),
         };
 
@@ -275,7 +274,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL 1.0.1 through 1.1.1, or LibreSSL 2.5
-through 3.4.x, but a different version of OpenSSL was found. The build is now aborting
+through 3.4.0, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
