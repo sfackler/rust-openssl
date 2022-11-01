0d2b5f924e94c608a83132c21f81186b9ac6ea29
Charlie Li <git@vishwin.info>
2022-11-01 14:40:37 -0400

    Allow LibreSSL 3.6.x, as 3.6.1 is declared stable

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -281,6 +281,7 @@ See rust-openssl documentation for more information:
             (3, 4, _) => ('3', '4', 'x'),
             (3, 5, _) => ('3', '5', 'x'),
             (3, 6, 0) => ('3', '6', '0'),
+            (3, 6, _) => ('3', '6', 'x'),
             _ => version_error(),
         };
 
@@ -323,7 +324,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL (version 1.0.1 through 1.1.1, or 3.0.0), or LibreSSL 2.5
-through 3.6.0, but a different version of OpenSSL was found. The build is now aborting
+through 3.6.x, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
