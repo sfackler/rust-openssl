ed227f78edb1410174878b891596a7153298b895
wuweichao.1 <wuweichao.1@bytedance.com>
2022-11-02 17:36:12 +0800

    支持从内存set证书

--- openssl/src/ssl/mod.rs
+++ openssl/src/ssl/mod.rs
@@ -2271,7 +2271,7 @@ impl fmt::Debug for SslRef {
 impl SslRef {
     #[cfg(feature = "tongsuo")]
     /// 只能在client hello callback中调用
-    pub fn get_client_cipher_list_name(&self) -> Vec<String> {
+    pub fn get_client_cipher_list_name(&mut self) -> Vec<String> {
         let mut lists = vec![];
         unsafe {
             let mut ptr = ptr::null();
@@ -2288,13 +2288,32 @@ impl SslRef {
         }
     }
     #[cfg(feature = "tongsuo")]
-    #[corresponds(SSL_Use_Private_Key_file)]
-    pub fn set_private_key_file<P: AsRef<Path>>(&self, path: P, ssl_file_type: SslFiletype) {
+    #[corresponds(SSL_use_Private_Key_file)]
+    pub fn set_private_key_file<P: AsRef<Path>>(&mut self, path: P, ssl_file_type: SslFiletype) {
         let key_file = CString::new(path.as_ref().as_os_str().to_str().unwrap()).unwrap();
         unsafe {
             ffi::SSL_use_PrivateKey_file(self.as_ptr(), key_file.as_ptr(), ssl_file_type.as_raw())
         };
     }
+    #[cfg(feature = "tongsuo")]
+    #[corresponds(SSL_use_PrivateKey)]
+    pub fn use_private_key_pem(&mut self, key: &[u8]) {
+        use crate::pkey;
+        let pkey = pkey::PKey::private_key_from_pem(key).unwrap();
+        unsafe {
+            ffi::SSL_use_PrivateKey(self.as_ptr(), pkey.as_ptr());
+        };
+    }
+    #[cfg(feature = "tongsuo")]
+    #[corresponds(SSL_use_certificate)]
+    pub fn use_certificate_pem(&mut self, cert: &[u8]) {
+        use crate::x509;
+        let cert = x509::X509::from_pem(cert).unwrap();
+        unsafe {
+            ffi::SSL_use_certificate(self.as_ptr(), cert.as_ptr());
+        };
+    }
+
     #[cfg(feature = "tongsuo")]
     #[corresponds(SSL_use_certificate_chain_file)]
     pub fn set_certificate_chain_file<P: AsRef<Path>>(&mut self, path: P) {
@@ -2304,8 +2323,31 @@ impl SslRef {
         };
     }
     #[cfg(feature = "tongsuo")]
+    pub fn use_ntls_key_content_and_cert_content_pem(
+        &mut self,
+        sign_private_key_content: &[u8],
+        sign_cert_content: &[u8],
+        enc_private_key_content: &[u8],
+        enc_cert_content: &[u8],
+    ) -> Result<(), ErrorStack> {
+        use crate::{pkey, x509};
+
+        //sign_private_key_content is not null, unwrap is safe
+        let sign_pkey = pkey::PKey::private_key_from_pem(sign_private_key_content).unwrap();
+        let sign_cert = x509::X509::from_pem(sign_cert_content)?;
+        let enc_pkey = pkey::PKey::private_key_from_pem(enc_private_key_content).unwrap();
+        let enc_cert = x509::X509::from_pem(enc_cert_content).unwrap();
+        unsafe {
+            ffi::SSL_use_sign_PrivateKey(self.as_ptr(), sign_pkey.as_ptr());
+            ffi::SSL_use_sign_certificate(self.as_ptr(), sign_cert.as_ptr());
+            ffi::SSL_use_enc_PrivateKey(self.as_ptr(), enc_pkey.as_ptr());
+            ffi::SSL_use_enc_certificate(self.as_ptr(), enc_cert.as_ptr());
+        };
+        Ok(())
+    }
+    #[cfg(feature = "tongsuo")]
     pub fn use_ntls_key_and_cert<P: AsRef<Path>>(
-        &self,
+        &mut self,
         sign_private_key_file: P,
         sign_cert_file: P,
         enc_private_key_file: P,
