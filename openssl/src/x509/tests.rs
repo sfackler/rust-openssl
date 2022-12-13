263c7ce1e694a3b2ed16e1d99acbba3cc5280edb
Steffen Eiden <seiden@linux.ibm.com>
2022-12-13 19:15:37 +0100

    Add X509_NAME_add_entry binding
    Signed-off-by: Steffen Eiden <seiden@linux.ibm.com>

--- openssl/src/x509/tests.rs
+++ openssl/src/x509/tests.rs
@@ -693,3 +693,21 @@ fn test_load_cert_file() {
         .init(&store, &cert, &chain, |c| c.verify_cert())
         .unwrap());
 }
+
+#[test]
+#[cfg(any(ossl101, libressl350))]
+fn test_add_name_entry() {
+    let cert = include_bytes!("../../test/cert.pem");
+    let cert = X509::from_pem(cert).unwrap();
+    let inp_name = cert.subject_name().entries().next().unwrap();
+
+    let mut names = X509Name::builder().unwrap();
+    names.append_entry(inp_name).unwrap();
+    let names = names.build();
+
+    let mut entries = names.entries();
+    let outp_name = entries.next().unwrap();
+    assert_eq!(outp_name.object().nid(), inp_name.object().nid());
+    assert_eq!(outp_name.data().as_slice(), inp_name.data().as_slice());
+    assert!(entries.next().is_none());
+}
