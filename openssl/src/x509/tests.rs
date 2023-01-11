29cc751df54523dbe76ca8a26fe6285b0fa564d4
Bernd Krietenstein <bernd.krietenstein@rohde-schwarz.com>
2023-01-11 16:11:00 +0100

    Fixed openssl version issues.

--- openssl/src/x509/tests.rs
+++ openssl/src/x509/tests.rs
@@ -23,7 +23,10 @@ use crate::x509::verify::{X509VerifyFlags, X509VerifyParam};
 #[cfg(ossl110)]
 use crate::x509::X509Builder;
 use crate::x509::{X509Name, X509Req, X509StoreContext, X509VerifyResult, X509};
-use crate::x509::{X509Purpose, X509PurposeId};
+#[cfg(any(ossl102, libressl261))]
+use crate::x509::X509Purpose;
+#[cfg(ossl102)]
+use crate::x509::X509PurposeId;
 use hex::{self, FromHex};
 #[cfg(any(ossl102, libressl261))]
 use libc::time_t;
