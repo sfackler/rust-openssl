d2e30181e586929abf1ee93d5c8152f8d034385c
Bernd Krietenstein <bernd.krietenstein@rohde-schwarz.com>
2023-01-13 16:17:48 +0100

    Fixed systest.

--- systest/build.rs
+++ systest/build.rs
@@ -108,7 +108,10 @@ fn main() {
             || s.starts_with("CRYPTO_EX_")
     });
     cfg.skip_struct(|s| {
-        s == "ProbeResult" || s == "X509_OBJECT_data" // inline union
+        s == "ProbeResult" ||
+        s == "X509_OBJECT_data" || // inline union
+        s == "PKCS7_data" ||
+        s == "ASN1_TYPE_value"
     });
     cfg.skip_fn(move |s| {
         s == "CRYPTO_memcmp" ||                 // uses volatile
@@ -128,7 +131,9 @@ fn main() {
     cfg.skip_field_type(|s, field| {
         (s == "EVP_PKEY" && field == "pkey") ||      // union
             (s == "GENERAL_NAME" && field == "d") || // union
-            (s == "X509_OBJECT" && field == "data") // union
+            (s == "X509_OBJECT" && field == "data") || // union
+            (s == "PKCS7" && field == "d") || // union
+            (s == "ASN1_TYPE" && field == "value") // union
     });
     cfg.skip_signededness(|s| {
         s.ends_with("_cb")
