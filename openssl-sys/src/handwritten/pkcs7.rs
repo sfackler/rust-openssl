f13427168389420bc21011903ddb21c9d59be351
Bernd Krietenstein <bernd.krietenstein@rohde-schwarz.com>
2023-02-27 09:44:10 +0100

    Removed unnecessary cfg_if's.

--- openssl-sys/src/handwritten/pkcs7.rs
+++ openssl-sys/src/handwritten/pkcs7.rs
@@ -8,92 +8,51 @@ pub struct PKCS7_CTX {
     propq: *mut c_char,
 }
 
-cfg_if! {
-    if #[cfg(any(ossl101, libressl251))] {
-        #[repr(C)]
-        pub struct PKCS7_SIGNED {
-            pub version: *mut ASN1_INTEGER, /* version 1 */
-            pub md_algs: *mut stack_st_X509_ALGOR, /* md used */
-            pub cert: *mut stack_st_X509, /* [ 0 ] */
-            pub crl: *mut stack_st_X509_CRL, /* [ 1 ] */
-            pub signer_info: *mut stack_st_PKCS7_SIGNER_INFO,
-            pub contents: *mut PKCS7,
-        }
-    } else {
-        pub enum PKCS7_SIGNED {}
-    }
+#[repr(C)]
+pub struct PKCS7_SIGNED {
+    pub version: *mut ASN1_INTEGER, /* version 1 */
+    pub md_algs: *mut stack_st_X509_ALGOR, /* md used */
+    pub cert: *mut stack_st_X509, /* [ 0 ] */
+    pub crl: *mut stack_st_X509_CRL, /* [ 1 ] */
+    pub signer_info: *mut stack_st_PKCS7_SIGNER_INFO,
+    pub contents: *mut PKCS7,
 }
-
-cfg_if! {
-    if #[cfg(any(ossl101, libressl251))] {
-        #[repr(C)]
-        pub struct PKCS7_ENC_CONTENT {
-            pub content_type: *mut ASN1_OBJECT,
-            pub algorithm: *mut X509_ALGOR,
-            pub enc_data: *mut ASN1_OCTET_STRING, /* [ 0 ] */
-            pub cipher: *const EVP_CIPHER,
-            #[cfg(ossl300)]
-            pub ctx: *const PKCS7_CTX,
-       }
-    } else {
-        pub enum PKCS7_ENC_CONTENT {}
-    }
+#[repr(C)]
+pub struct PKCS7_ENC_CONTENT {
+    pub content_type: *mut ASN1_OBJECT,
+    pub algorithm: *mut X509_ALGOR,
+    pub enc_data: *mut ASN1_OCTET_STRING, /* [ 0 ] */
+    pub cipher: *const EVP_CIPHER,
+    #[cfg(ossl300)]
+    pub ctx: *const PKCS7_CTX,
 }
-
-cfg_if! {
-    if #[cfg(any(ossl101, libressl251))] {
-        #[repr(C)]
-        pub struct PKCS7_ENVELOPE {
-            pub version: *mut ASN1_INTEGER, /* version 0 */
-            pub recipientinfo: *mut stack_st_PKCS7_RECIP_INFO,
-            pub enc_data: *mut PKCS7_ENC_CONTENT,
-        }
-    }  else {
-        pub enum PKCS7_ENVELOPE {}
-    }
+#[repr(C)]
+pub struct PKCS7_ENVELOPE {
+    pub version: *mut ASN1_INTEGER, /* version 0 */
+    pub recipientinfo: *mut stack_st_PKCS7_RECIP_INFO,
+    pub enc_data: *mut PKCS7_ENC_CONTENT,
 }
-
-cfg_if! {
-    if #[cfg(any(ossl101, libressl251))] {
-        #[repr(C)]
-        pub struct PKCS7_SIGN_ENVELOPE {
-            pub version: *mut ASN1_INTEGER, /* version 1 */
-            pub md_algs: *mut stack_st_X509_ALGOR, /* md used */
-            pub cert: *mut stack_st_X509, /* [ 0 ] */
-            pub crl: *mut stack_st_X509_CRL, /* [ 1 ] */
-            pub signer_info: *mut stack_st_PKCS7_SIGNER_INFO,
-            pub enc_data: *mut PKCS7_ENC_CONTENT,
-            pub recipientinfo: *mut stack_st_PKCS7_RECIP_INFO
-        }
-    } else {
-        pub enum PKCS7_SIGN_ENVELOPE {}
-    }
+#[repr(C)]
+pub struct PKCS7_SIGN_ENVELOPE {
+    pub version: *mut ASN1_INTEGER, /* version 1 */
+    pub md_algs: *mut stack_st_X509_ALGOR, /* md used */
+    pub cert: *mut stack_st_X509, /* [ 0 ] */
+    pub crl: *mut stack_st_X509_CRL, /* [ 1 ] */
+    pub signer_info: *mut stack_st_PKCS7_SIGNER_INFO,
+    pub enc_data: *mut PKCS7_ENC_CONTENT,
+    pub recipientinfo: *mut stack_st_PKCS7_RECIP_INFO
 }
-
-cfg_if! {
-    if #[cfg(any(ossl101, libressl251))] {
-        #[repr(C)]
-        pub struct PKCS7_DIGEST {
-            pub version: *mut ASN1_INTEGER, /* version 0 */
-            pub md: *mut X509_ALGOR, /* md used */
-            pub contents: *mut PKCS7,
-            pub digest: *mut ASN1_OCTET_STRING,
-        }
-    } else {
-        pub enum PKCS7_DIGEST {}
-    }
+#[repr(C)]
+pub struct PKCS7_DIGEST {
+    pub version: *mut ASN1_INTEGER, /* version 0 */
+    pub md: *mut X509_ALGOR, /* md used */
+    pub contents: *mut PKCS7,
+    pub digest: *mut ASN1_OCTET_STRING,
 }
-
-cfg_if! {
-    if #[cfg(any(ossl101, libressl251))] {
-        #[repr(C)]
-        pub struct PKCS7_ENCRYPT {
-            pub version: *mut ASN1_INTEGER, /* version 0 */
-            pub enc_data: *mut PKCS7_ENC_CONTENT,
-        }
-    } else {
-        pub enum PKCS7_ENCRYPT {}
-    }
+#[repr(C)]
+pub struct PKCS7_ENCRYPT {
+    pub version: *mut ASN1_INTEGER, /* version 0 */
+    pub enc_data: *mut PKCS7_ENC_CONTENT,
 }
 
 extern "C" {
@@ -108,84 +67,67 @@ extern "C" {
     pub fn PKCS7_RECIP_INFO_free(info: *mut PKCS7_RECIP_INFO);
 }
 
-cfg_if! {
-    if #[cfg(any(ossl101, libressl251))] {
-        #[repr(C)]
-        pub struct PKCS7 {
-            /*
-             * The following is non NULL if it contains ASN1 encoding of this
-             * structure
-             */
-            pub asn1: *mut c_uchar,
-            pub length: c_long,
-            // # define PKCS7_S_HEADER  0
-            // # define PKCS7_S_BODY    1
-            // # define PKCS7_S_TAIL    2
-            pub state: c_int, /* used during processing */
-            pub detached: c_int,
-            pub type_: *mut ASN1_OBJECT,
-            /* content as defined by the type */
-            /*
-             * all encryption/message digests are applied to the 'contents', leaving
-             * out the 'type' field.
-             */
-            pub d: PKCS7_data,
-            #[cfg(ossl300)]
-            pub ctx: PKCS7_CTX,
-        }
-        #[repr(C)]
-        pub union PKCS7_data {
-            pub ptr: *mut c_char,
-            /* NID_pkcs7_data */
-            pub data: *mut ASN1_OCTET_STRING,
-            /* NID_pkcs7_signed */
-            pub sign: *mut PKCS7_SIGNED,
-            /* NID_pkcs7_enveloped */
-            pub enveloped: *mut PKCS7_ENVELOPE,
-            /* NID_pkcs7_signedAndEnveloped */
-            pub signed_and_enveloped: *mut PKCS7_SIGN_ENVELOPE,
-            /* NID_pkcs7_digest */
-            pub digest: *mut PKCS7_DIGEST,
-            /* NID_pkcs7_encrypted */
-            pub encrypted: *mut PKCS7_ENCRYPT,
-            /* Anything else */
-            pub other: *mut ASN1_TYPE,
-        }
-    } else {
-         pub enum PKCS7 {}
-    }
+#[repr(C)]
+pub struct PKCS7 {
+    /*
+     * The following is non NULL if it contains ASN1 encoding of this
+     * structure
+     */
+    pub asn1: *mut c_uchar,
+    pub length: c_long,
+    // # define PKCS7_S_HEADER  0
+    // # define PKCS7_S_BODY    1
+    // # define PKCS7_S_TAIL    2
+    pub state: c_int, /* used during processing */
+    pub detached: c_int,
+    pub type_: *mut ASN1_OBJECT,
+    /* content as defined by the type */
+    /*
+     * all encryption/message digests are applied to the 'contents', leaving
+     * out the 'type' field.
+     */
+    pub d: PKCS7_data,
+    #[cfg(ossl300)]
+    pub ctx: PKCS7_CTX,
 }
 
-cfg_if! {
-    if #[cfg(any(ossl101, libressl))] {
-        #[repr(C)]
-        pub struct PKCS7_ISSUER_AND_SERIAL {
-            pub issuer: *mut X509_NAME,
-            pub serial: *mut ASN1_INTEGER,
-        }
-    } else {
-        pub enum PKCS7_ISSUER_AND_SERIAL {}
-    }
+#[repr(C)]
+pub union PKCS7_data {
+    pub ptr: *mut c_char,
+    /* NID_pkcs7_data */
+    pub data: *mut ASN1_OCTET_STRING,
+    /* NID_pkcs7_signed */
+    pub sign: *mut PKCS7_SIGNED,
+    /* NID_pkcs7_enveloped */
+    pub enveloped: *mut PKCS7_ENVELOPE,
+    /* NID_pkcs7_signedAndEnveloped */
+    pub signed_and_enveloped: *mut PKCS7_SIGN_ENVELOPE,
+    /* NID_pkcs7_digest */
+    pub digest: *mut PKCS7_DIGEST,
+    /* NID_pkcs7_encrypted */
+    pub encrypted: *mut PKCS7_ENCRYPT,
+    /* Anything else */
+    pub other: *mut ASN1_TYPE,
 }
 
-cfg_if! {
-    if #[cfg(any(ossl101, libressl))] {
-        #[repr(C)]
-        pub struct PKCS7_SIGNER_INFO {
-            pub version: *mut ASN1_INTEGER, /* version 1 */
-            pub issuer_and_serial: *mut PKCS7_ISSUER_AND_SERIAL,
-            pub digest_alg: *mut X509_ALGOR,
-            pub auth_attr: *mut stack_st_X509_ATTRIBUTE, /* [ 0 ] */
-            pub digest_enc_alg: *mut X509_ALGOR,
-            pub enc_digest: *mut ASN1_OCTET_STRING,
-            pub unauth_attr: *mut stack_st_X509_ATTRIBUTE, /* [ 1 ] */
-            pub pkey: *mut EVP_PKEY, /* The private key to sign with */
-            #[cfg(ossl300)]
-            pub ctx: *const PKCS7_CTX,
-        }
-    } else {
-        pub enum PKCS7_SIGNER_INFO {}
-    }
+#[repr(C)]
+pub struct PKCS7_ISSUER_AND_SERIAL {
+    pub issuer: *mut X509_NAME,
+    pub serial: *mut ASN1_INTEGER,
+}
+
+#[repr(C)]
+pub struct PKCS7_SIGNER_INFO {
+    pub version: *mut ASN1_INTEGER, /* version 1 */
+    pub issuer_and_serial: *mut PKCS7_ISSUER_AND_SERIAL,
+    pub digest_alg: *mut X509_ALGOR,
+    pub auth_attr: *mut stack_st_X509_ATTRIBUTE, /* [ 0 ] */
+    pub digest_enc_alg: *mut X509_ALGOR,
+    pub enc_digest: *mut ASN1_OCTET_STRING,
+    pub unauth_attr: *mut stack_st_X509_ATTRIBUTE, /* [ 1 ] */
+    pub pkey: *mut EVP_PKEY, /* The private key to sign with */
+    #[cfg(ossl300)]
+    pub ctx: *const PKCS7_CTX,
 }
 
 stack!(stack_st_PKCS7_SIGNER_INFO);
