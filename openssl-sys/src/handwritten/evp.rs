8f23c2f6fa527657fa4d98cd6ac808d301d1aae7
Paul Kehrer <paul.l.kehrer@gmail.com>
2023-04-22 16:13:48 -0600

    binding to get fips status for ossl300

--- openssl-sys/src/handwritten/evp.rs
+++ openssl-sys/src/handwritten/evp.rs
@@ -65,6 +65,14 @@ cfg_if! {
     }
 }
 
+cfg_if! {
+    if #[cfg(ossl300)] {
+        extern "C" {
+            pub fn EVP_default_properties_is_fips_enabled(libctx: *mut OSSL_LIB_CTX) -> c_int;
+        }
+    }
+}
+
 extern "C" {
     pub fn EVP_DigestInit_ex(ctx: *mut EVP_MD_CTX, typ: *const EVP_MD, imple: *mut ENGINE)
         -> c_int;
