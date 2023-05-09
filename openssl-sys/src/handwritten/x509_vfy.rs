890bca5109e7f15c18ccf8fb95967ef18f9115b6
Sven Herrmann <sven.herrmann@posteo.de>
2023-05-09 18:42:05 +0200

    add X509_STORE_add_crl

--- openssl-sys/src/handwritten/x509_vfy.rs
+++ openssl-sys/src/handwritten/x509_vfy.rs
@@ -44,6 +44,7 @@ extern "C" {
     pub fn X509_STORE_CTX_cleanup(ctx: *mut X509_STORE_CTX);
 
     pub fn X509_STORE_add_cert(store: *mut X509_STORE, x: *mut X509) -> c_int;
+    pub fn X509_STORE_add_crl(store: *mut X509_STORE, x: *mut X509_CRL) -> c_int;
 
     pub fn X509_STORE_set_default_paths(store: *mut X509_STORE) -> c_int;
     pub fn X509_STORE_set_flags(store: *mut X509_STORE, flags: c_ulong) -> c_int;
