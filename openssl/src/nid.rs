0b2e5291294e5319d9079c8481fe0ee8d87f53a1
Yohann <yohann.paulus@gmail.com>
2023-05-26 21:47:38 +0200

    Adding nid support for x25519

--- openssl/src/nid.rs
+++ openssl/src/nid.rs
@@ -1074,6 +1074,9 @@ impl Nid {
     pub const AES_128_CBC_HMAC_SHA1: Nid = Nid(ffi::NID_aes_128_cbc_hmac_sha1);
     pub const AES_192_CBC_HMAC_SHA1: Nid = Nid(ffi::NID_aes_192_cbc_hmac_sha1);
     pub const AES_256_CBC_HMAC_SHA1: Nid = Nid(ffi::NID_aes_256_cbc_hmac_sha1);
+    pub const X25519: Nid = Nid(ffi::NID_X25519);
+
+
     #[cfg(any(ossl111, libressl291))]
     pub const SM3: Nid = Nid(ffi::NID_sm3);
     #[cfg(ossl111)]
