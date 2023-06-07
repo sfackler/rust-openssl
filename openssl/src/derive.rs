2604033874debae65cad42ecef47613f6a147e85
Paul Kehrer <paul.l.kehrer@gmail.com>
2023-06-07 10:21:03 +0800

    remove outdated comment

--- openssl/src/derive.rs
+++ openssl/src/derive.rs
@@ -83,10 +83,6 @@ impl<'a> Deriver<'a> {
     }
 
     /// Sets the peer key used for secret derivation.
-    ///
-    /// This corresponds to [`EVP_PKEY_derive_set_peer`]:
-    ///
-    /// [`EVP_PKEY_derive_set_peer`]: https://www.openssl.org/docs/manmaster/crypto/EVP_PKEY_derive_init.html
     #[corresponds(EVP_PKEY_derive_set_peer)]
     pub fn set_peer<T>(&mut self, key: &'a PKeyRef<T>) -> Result<(), ErrorStack>
     where
