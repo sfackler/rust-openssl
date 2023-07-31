31f14765679775ceb077f137ca2aeee9a6c43885
Brian Chrzanowski <chrzanowskitest@gmail.com>
2023-07-31 17:37:40 -0400

    fixed bindings

--- openssl-sys/src/handwritten/engine.rs
+++ openssl-sys/src/handwritten/engine.rs
@@ -1,8 +1,6 @@
 use super::super::*;
 use libc::*;
 
-pub enum ENGINE {}
-
 pub enum RSA_METHOD {}
 pub enum DSA_METHOD {}
 pub enum DH_METHOD {}
@@ -16,22 +14,34 @@ pub enum ENGINE_CTRL_FUNC_PTR {}
 pub enum ENGINE_LOAD_KEY_PTR {}
 pub enum UI_METHOD {}
 
+pub const ENGINE_METHOD_RSA: u32 = 0x0001;
+pub const ENGINE_METHOD_DSA: u32 = 0x0002;
+pub const ENGINE_METHOD_DH: u32 = 0x0004;
+pub const ENGINE_METHOD_RAND: u32 = 0x0008;
+pub const ENGINE_METHOD_CIPHERS: u32 = 0x0040;
+pub const ENGINE_METHOD_DIGESTS: u32 = 0x0080;
+pub const ENGINE_METHOD_PKEY_METHS: u32 = 0x0200;
+pub const ENGINE_METHOD_PKEY_ASN1_METHS: u32 = 0x0400;
+pub const ENGINE_METHOD_EC: u32 = 0x0800;
+
+pub const ENGINE_METHOD_ALL: u32 = 0xffff;
+pub const ENGINE_METHOD_NONE: u32 = 0xffff;
+
+#[cfg(ossl110)]
 extern "C" {
-    // NOTE some of these _may_ need to be mutable...
+    pub fn ENGINE_get_first() -> *mut ENGINE;
 
-    pub fn ENGINE_get_first() -> *const ENGINE;
+    pub fn ENGINE_get_last() -> *mut ENGINE;
 
-    pub fn ENGINE_get_last() -> *const ENGINE;
+    pub fn ENGINE_get_next(e: *mut ENGINE) -> *mut ENGINE;
 
-    pub fn ENGINE_get_next(e: *const ENGINE) -> *const ENGINE;
+    pub fn ENGINE_get_prev(e: *mut ENGINE) -> *mut ENGINE;
 
-    pub fn ENGINE_get_prev(e: *const ENGINE) -> *const ENGINE;
+    pub fn ENGINE_add(e: *mut ENGINE) -> c_int;
 
-    pub fn ENGINE_add(e: *const ENGINE) -> c_int;
+    pub fn ENGINE_remove(e: *mut ENGINE) -> c_int;
 
-    pub fn ENGINE_remove(e: *const ENGINE) -> c_int;
-
-    pub fn ENGINE_by_id(id: *const c_char) -> *const ENGINE;
+    pub fn ENGINE_by_id(id: *const c_char) -> *mut ENGINE;
 
     pub fn ENGINE_init(e: *mut ENGINE) -> c_int;
 
@@ -39,12 +49,12 @@ extern "C" {
 
     pub fn ENGINE_load_builtin_engines();
 
-    pub fn ENGINE_get_default_RSA() -> *const ENGINE;
-    pub fn ENGINE_get_default_DSA() -> *const ENGINE;
-    pub fn ENGINE_get_default_DH() -> *const ENGINE;
-    pub fn ENGINE_get_default_RAND() -> *const ENGINE;
-    pub fn ENGINE_get_cipher_engine(nid: c_int) -> *const ENGINE;
-    pub fn ENGINE_get_digest_engine(nid: c_int) -> *const ENGINE;
+    pub fn ENGINE_get_default_RSA() -> *mut ENGINE;
+    pub fn ENGINE_get_default_DSA() -> *mut ENGINE;
+    pub fn ENGINE_get_default_DH() -> *mut ENGINE;
+    pub fn ENGINE_get_default_RAND() -> *mut ENGINE;
+    pub fn ENGINE_get_cipher_engine(nid: c_int) -> *mut ENGINE;
+    pub fn ENGINE_get_digest_engine(nid: c_int) -> *mut ENGINE;
 
     pub fn ENGINE_set_default_RSA(e: *mut ENGINE) -> c_int;
     pub fn ENGINE_set_default_DSA(e: *mut ENGINE) -> c_int;
@@ -84,14 +94,14 @@ extern "C" {
     pub fn ENGINE_register_all_digests();
 
     pub fn ENGINE_register_complete(e: *mut ENGINE) -> c_int;
-    pub fn ENGINE_register_all_complete(e: *mut ENGINE) -> c_int;
+    pub fn ENGINE_register_all_complete() -> c_int;
 
     pub fn ENGINE_ctrl(
         e: *mut ENGINE,
         cmd: c_int,
         i: c_long,
-        p: *const c_void,
-        freefunc: i32,
+        p: *mut c_void,
+        f: extern "C" fn (),
     ) -> c_int;
 
     pub fn ENGINE_cmd_is_executable(e: *mut ENGINE, cmd: c_int) -> c_int;
@@ -100,8 +110,8 @@ extern "C" {
         e: *mut ENGINE,
         cmd_name: *const c_char,
         i: c_long,
-        p: *const c_void,
-        freefunc: i32,
+        p: *mut c_void,
+        f: extern "C" fn (),
         cmd_optional: c_int,
     ) -> c_int;
 
@@ -130,46 +140,45 @@ extern "C" {
 
     pub fn ENGINE_set_RAND(e: *mut ENGINE, RAND_meth: *const RAND_METHOD) -> c_int;
 
-    // TODO pass a different function pointer for these!!?!?!?
     pub fn ENGINE_set_destroy_function(
         e: *mut ENGINE,
-        destroy_f: *const ENGINE_GEN_INT_FUNC_PTR,
+        destroy_f: extern "C" fn (*mut ENGINE) -> c_int,
     ) -> c_int;
 
     pub fn ENGINE_set_init_function(
         e: *mut ENGINE,
-        init_f: *const ENGINE_GEN_INT_FUNC_PTR,
+        init_f: extern "C" fn (*mut ENGINE) -> c_int,
     ) -> c_int;
 
     pub fn ENGINE_set_finish_function(
         e: *mut ENGINE,
-        finish_f: *const ENGINE_GEN_INT_FUNC_PTR,
+        finish_f: extern "C" fn (*mut ENGINE) -> c_int,
     ) -> c_int;
 
     pub fn ENGINE_set_ctrl_function(
         e: *mut ENGINE,
-        ctrl_f: *const ENGINE_GEN_INT_FUNC_PTR,
+        ctrl_f: extern "C" fn (*mut ENGINE, c_int, c_long, *mut c_void, extern "C" fn ()) -> c_int,
     ) -> c_int;
 
     pub fn ENGINE_set_load_privkey_function(
         e: *mut ENGINE,
-        loadpriv_f: *const ENGINE_GEN_INT_FUNC_PTR,
+        loadpriv_f: extern "C" fn (*mut ENGINE, *const c_char, *mut UI_METHOD, *mut c_void) -> *mut EVP_PKEY,
     ) -> c_int;
 
     pub fn ENGINE_set_load_pubkey_function(
         e: *mut ENGINE,
-        loadpub_f: *const ENGINE_GEN_INT_FUNC_PTR,
+        loadpub_f: unsafe extern "C" fn (*mut ENGINE, *const c_char, *mut UI_METHOD, *mut c_void) -> *mut EVP_PKEY,
     ) -> c_int;
 
-    pub fn ENGINE_set_ciphers(e: *mut ENGINE, f: *const ENGINE_CIPHERS_PTR) -> c_int;
+    pub fn ENGINE_set_ciphers(e: *mut ENGINE, f: ENGINE_CIPHERS_PTR) -> c_int;
 
-    pub fn ENGINE_set_digests(e: *mut ENGINE, f: *const ENGINE_DIGESTS_PTR) -> c_int;
+    pub fn ENGINE_set_digests(e: *mut ENGINE, f: ENGINE_DIGESTS_PTR) -> c_int;
 
     pub fn ENGINE_set_cmd_defns(e: *mut ENGINE, defns: *const ENGINE_CMD_DEFN) -> c_int;
 
-    pub fn ENGINE_get_id(e: *const ENGINE) -> *mut c_char;
+    pub fn ENGINE_get_id(e: *const ENGINE) -> *const c_char;
 
-    pub fn ENGINE_get_name(e: *const ENGINE) -> *mut c_char;
+    pub fn ENGINE_get_name(e: *const ENGINE) -> *const c_char;
 
     pub fn ENGINE_get_RSA(e: *const ENGINE) -> *const RSA_METHOD;
 
@@ -179,44 +188,47 @@ extern "C" {
 
     pub fn ENGINE_get_RAND(e: *const ENGINE) -> *const RAND_METHOD;
 
-    pub fn ENGINE_get_destroy_function(e: *const ENGINE) -> *const ENGINE_GEN_INT_FUNC_PTR;
+    pub fn ENGINE_get_destroy_function(e: *const ENGINE) -> ENGINE_GEN_INT_FUNC_PTR;
 
-    pub fn ENGINE_get_init_function(e: *const ENGINE) -> *const ENGINE_GEN_INT_FUNC_PTR;
+    pub fn ENGINE_get_init_function(e: *const ENGINE) -> ENGINE_GEN_INT_FUNC_PTR;
 
-    pub fn ENGINE_get_finish_function(e: *const ENGINE) -> *const ENGINE_GEN_INT_FUNC_PTR;
+    pub fn ENGINE_get_finish_function(e: *const ENGINE) -> ENGINE_GEN_INT_FUNC_PTR;
 
-    pub fn ENGINE_get_ctrl_function(e: *const ENGINE) -> *const ENGINE_CTRL_FUNC_PTR;
+    pub fn ENGINE_get_ctrl_function(e: *const ENGINE) -> ENGINE_CTRL_FUNC_PTR;
 
-    pub fn ENGINE_get_load_privkey_function(e: *const ENGINE) -> *const ENGINE_LOAD_KEY_PTR;
+    pub fn ENGINE_get_load_privkey_function(e: *const ENGINE) -> ENGINE_LOAD_KEY_PTR;
 
-    pub fn ENGINE_get_load_pubkey_function(e: *const ENGINE) -> *const ENGINE_LOAD_KEY_PTR;
+    pub fn ENGINE_get_load_pubkey_function(e: *const ENGINE) -> ENGINE_LOAD_KEY_PTR;
 
-    pub fn ENGINE_get_ciphers(e: *const ENGINE) -> *const ENGINE_CIPHERS_PTR;
+    pub fn ENGINE_get_ciphers(e: *const ENGINE) -> ENGINE_CIPHERS_PTR;
 
-    pub fn ENGINE_get_digests(e: *const ENGINE) -> *const ENGINE_DIGESTS_PTR;
+    pub fn ENGINE_get_digests(e: *const ENGINE) -> ENGINE_DIGESTS_PTR;
 
-    pub fn ENGINE_get_cipher(e: *const ENGINE, nid: c_int) -> *const EVP_CIPHER;
+    pub fn ENGINE_get_cipher(e: *mut ENGINE, nid: c_int) -> *const EVP_CIPHER;
 
-    pub fn ENGINE_get_digest(e: *const ENGINE, nid: c_int) -> *const EVP_MD;
+    pub fn ENGINE_get_digest(e: *mut ENGINE, nid: c_int) -> *const EVP_MD;
 
     pub fn ENGINE_get_flags(e: *const ENGINE) -> c_int;
 
     pub fn ENGINE_get_cmd_defns(e: *const ENGINE) -> *const ENGINE_CMD_DEFN;
 
     pub fn ENGINE_load_private_key(
-        e: *const ENGINE,
+        e: *mut ENGINE,
         key_id: *const c_char,
-        ui_method: *const UI_METHOD,
-        callback_data: *const c_void,
-    ) -> *const EVP_PKEY;
+        ui_method: *mut UI_METHOD,
+        callback_data: *mut c_void,
+    ) -> *mut EVP_PKEY;
 
     pub fn ENGINE_load_public_key(
-        e: *const ENGINE,
+        e: *mut ENGINE,
         key_id: *const c_char,
-        ui_method: *const UI_METHOD,
-        callback_data: *const c_void,
-    ) -> *const EVP_PKEY;
+        ui_method: *mut UI_METHOD,
+        callback_data: *mut c_void,
+    ) -> *mut EVP_PKEY;
 
-    #[cfg(ossl110)]
-    pub fn ENGINE_cleanup();
 }
+
+// extern "C" {
+//     #[cfg(not(any(ossl100)))]
+//     pub fn ENGINE_cleanup();
+// }
