9cafc739d2bc92ca3333504539e2c4d5787be810
David Benjamin <davidben@google.com>
2023-12-02 16:57:15 -0500

    Restore struct in handwritten, memory-unsafe bindings

--- openssl-sys/src/handwritten/x509.rs
+++ openssl-sys/src/handwritten/x509.rs
@@ -687,7 +687,17 @@ extern "C" {
     pub fn X509_REQ_print(bio: *mut BIO, req: *mut X509_REQ) -> c_int;
 }
 
-pub enum X509_PURPOSE {}
+#[repr(C)]
+pub struct X509_PURPOSE {
+    pub purpose: c_int,
+    pub trust: c_int, // Default trust ID
+    pub flags: c_int,
+    pub check_purpose:
+        Option<unsafe extern "C" fn(*const X509_PURPOSE, *const X509, c_int) -> c_int>,
+    pub name: *mut c_char,
+    pub sname: *mut c_char,
+    pub usr_data: *mut c_void,
+}
 
 const_ptr_api! {
     extern "C" {
