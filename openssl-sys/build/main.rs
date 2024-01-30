16b8858b4a4b0f0ea4895e7e08d7a6219aa35df0
Alex Gaynor <alex.gaynor@gmail.com>
2024-01-30 17:52:02 -0500

    Cleanup some not-required Path::new invocations

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -9,7 +9,7 @@ extern crate vcpkg;
 use std::collections::HashSet;
 use std::env;
 use std::ffi::OsString;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 mod cfgs;
 
 mod find_normal;
@@ -80,10 +80,10 @@ fn main() {
 
     let (lib_dirs, include_dir) = find_openssl(&target);
 
-    if !lib_dirs.iter().all(|p| Path::new(p).exists()) {
+    if !lib_dirs.iter().all(|p| p.exists()) {
         panic!("OpenSSL library directory does not exist: {:?}", lib_dirs);
     }
-    if !Path::new(&include_dir).exists() {
+    if !include_dir.exists() {
         panic!(
             "OpenSSL include directory does not exist: {}",
             include_dir.to_string_lossy()
