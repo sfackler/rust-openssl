a53177ec19ea98b64f8eae5b8d625cc75a1bde39
Georg Weisert <georg@freshx.de>
2024-03-03 12:18:23 +0100

    add conditional compilation directives

--- openssl/src/x509/mod.rs
+++ openssl/src/x509/mod.rs
@@ -1876,6 +1876,7 @@ impl X509Crl {
         unsafe {
             let crl = Self(cvt_p(ffi::X509_CRL_new())?);
 
+            #[cfg(ossl110)]
             if issuer_cert.version() >= Self::X509_VERSION_3 {
                 #[cfg(any(ossl110, libressl251, boringssl))]
                 {
@@ -1926,6 +1927,9 @@ impl X509Crl {
         }
     }
 
+    /// Note that `0` return value stands for version 1, `1` for version 2.
+    #[cfg(ossl110)]
+    #[corresponds(X509_CRL_get_version)]
     pub fn version(&self) -> i32 {
         unsafe { ffi::X509_CRL_get_version(self.as_ptr()) as i32 }
     }
@@ -2021,6 +2025,7 @@ impl X509Crl {
     /// This is an internal function, therefore the caller is expected to ensure not to call this with a CRLv1
     /// Set the crl_number extension's value.
     /// If the extension is not present, it will be added.
+    #[cfg(ossl110)]
     fn set_crl_number(&mut self, value: &BigNum) -> Result<(), ErrorStack> {
         debug_assert_eq!(self.version(), Self::X509_CRL_VERSION_2);
         unsafe {
@@ -2040,6 +2045,7 @@ impl X509Crl {
     /// Increment the crl number (or try to add the extension if not present)
     ///
     /// Returns the new crl number, unless self is a crlv1, which does not support extensions
+    #[cfg(ossl110)]
     pub fn increment_crl_number(&mut self) -> Result<Option<BigNum>, ErrorStack> {
         if self.version() == Self::X509_CRL_VERSION_2 {
             let new_crl_number = if let Some(mut n) = self.read_crl_number()? {
