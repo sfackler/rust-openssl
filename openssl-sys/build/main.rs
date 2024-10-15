f9027b7549e7d557d627aeadc157097b2c5c018b
Theo Buehler <tb@openbsd.org>
2024-10-15 08:05:19 +0200

    LibreSSL 4.0.0 is released & stable

--- openssl-sys/build/main.rs
+++ openssl-sys/build/main.rs
@@ -380,6 +380,8 @@ See rust-openssl documentation for more information:
             (3, 8, _) => ('3', '8', 'x'),
             (3, 9, 0) => ('3', '9', '0'),
             (3, 9, _) => ('3', '9', 'x'),
+            (4, 0, 0) => ('4', '0', '0'),
+            (4, 0, _) => ('4', '0', 'x'),
             _ => version_error(),
         };
 
@@ -422,7 +424,7 @@ fn version_error() -> ! {
         "
 
 This crate is only compatible with OpenSSL (version 1.0.1 through 1.1.1, or 3), or LibreSSL 2.5
-through 3.9.x, but a different version of OpenSSL was found. The build is now aborting
+through 4.0.x, but a different version of OpenSSL was found. The build is now aborting
 due to this version mismatch.
 
 "
