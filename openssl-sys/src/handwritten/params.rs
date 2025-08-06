dda046b5a74f6b6d7cdec73ffab51496e172f6be
Huw Jones <huw@pexip.com>
2025-08-06 14:51:24 +0100

    sys/params: add OSSL_PARAM dup, free, merge, locate, locate_const

--- openssl-sys/src/handwritten/params.rs
+++ openssl-sys/src/handwritten/params.rs
@@ -3,6 +3,11 @@ use libc::*;
 
 extern "C" {
     pub fn OSSL_PARAM_free(p: *mut OSSL_PARAM);
+    pub fn OSSL_PARAM_dup(params: *const OSSL_PARAM) -> *mut OSSL_PARAM;
+    pub fn OSSL_PARAM_merge(
+        params: *const OSSL_PARAM,
+        params1: *const OSSL_PARAM,
+    ) -> *mut OSSL_PARAM;
     pub fn OSSL_PARAM_construct_uint(key: *const c_char, buf: *mut c_uint) -> OSSL_PARAM;
     pub fn OSSL_PARAM_construct_end() -> OSSL_PARAM;
     pub fn OSSL_PARAM_construct_octet_string(
@@ -12,6 +17,10 @@ extern "C" {
     ) -> OSSL_PARAM;
 
     pub fn OSSL_PARAM_locate(p: *mut OSSL_PARAM, key: *const c_char) -> *mut OSSL_PARAM;
+    pub fn OSSL_PARAM_locate_const(
+        params: *const OSSL_PARAM,
+        key: *const c_char,
+    ) -> *const OSSL_PARAM;
     pub fn OSSL_PARAM_get_BN(p: *const OSSL_PARAM, val: *mut *mut BIGNUM) -> c_int;
     pub fn OSSL_PARAM_get_utf8_string(
         p: *const OSSL_PARAM,
