20091b38d02092047b6b9e4299135ace9a2a8070
Christopher Swenson <chswenson@microsoft.com>
2025-08-31 21:00:26 -0700

    Create separate type for the OSSL_PARAM array and a reference to an interior element

--- openssl/src/ossl_param.rs
+++ openssl/src/ossl_param.rs
@@ -44,15 +44,35 @@ impl OsslParamArrayRef {
     /// to it.
     #[corresponds(OSSL_PARAM_locate)]
     #[allow(dead_code)] // TODO: remove when when used by ML-DSA / ML-KEM
-    pub(crate) fn locate(&self, key: &CStr) -> Result<&OsslParamArrayRef, ErrorStack> {
+    pub(crate) fn locate<'a>(&'a self, key: &CStr) -> Result<&'a OsslParam<'a>, ErrorStack> {
         unsafe {
             let param = cvt_p(ffi::OSSL_PARAM_locate(self.as_ptr(), key.as_ptr()))?;
-            Ok(OsslParamArrayRef::from_ptr(param))
+            Ok(OsslParam::from_ptr(param))
         }
     }
+}
 
-    /// Get `BigNum` from the current OSSL_PARAM at the top of this
-    /// `OsslParamArray` reference.
+/// A reference to a single `OSSL_PARAM` structure inside
+/// an `OsslParamArray`.
+pub(crate) struct OsslParam<'a>(&'a ffi::OSSL_PARAM);
+
+impl<'a> OsslParam<'a> {
+    /// Constructs a reference to a single `OSSL_PARAM` structure
+    /// from a pointer to an interior element of an OSSL_PARAM array.
+    ///
+    /// # Safety
+    ///
+    /// The pointer must be valid and point to an `OSSL_PARAM` structure.
+    unsafe fn from_ptr(ptr: *const ffi::OSSL_PARAM) -> &'a OsslParam<'a> {
+        &*(ptr as *const OsslParam<'a>)
+    }
+
+    /// Returns a pointer to the underlying `OSSL_PARAM` structure.
+    unsafe fn as_ptr(&self) -> *const ffi::OSSL_PARAM {
+        self.0 as *const ffi::OSSL_PARAM
+    }
+
+    /// Get `BigNum` from this underlying OSSL_PARAM.
     #[corresponds(OSSL_PARAM_get_BN)]
     #[allow(dead_code)] // TODO: remove when when used by ML-DSA / ML-KEM
     pub(crate) fn get_bn(&self) -> Result<BigNum, ErrorStack> {
@@ -63,8 +83,7 @@ impl OsslParamArrayRef {
         }
     }
 
-    /// Get `&str` from the current OSSL_PARAM at the top of this
-    /// `OsslParamArray` reference.
+    /// Get `&str` from this underlying OSSL_PARAM.
     #[corresponds(OSSL_PARAM_get_utf8_string)]
     #[allow(dead_code)] // TODO: remove when when used by ML-DSA / ML-KEM
     pub(crate) fn get_utf8_string(&self) -> Result<&str, ErrorStack> {
@@ -75,8 +94,7 @@ impl OsslParamArrayRef {
         }
     }
 
-    /// Get octet string (as `&[u8]) from the current OSSL_PARAM at the
-    /// top of this `OsslParamArray` reference.
+    /// Get octet string (as `&[u8]) from this underlying OSSL_PARAM.
     #[corresponds(OSSL_PARAM_get_octet_string)]
     #[allow(dead_code)] // TODO: remove when when used by ML-DSA / ML-KEM
     pub(crate) fn get_octet_string(&self) -> Result<&[u8], ErrorStack> {
